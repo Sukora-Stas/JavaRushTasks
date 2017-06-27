ab22c2bc9d6096c11be914e5f508aa8db3318dff
Sukora-Stas <stasiksukora@mail.ru>
2017-06-27 16:32:19 +0300

    task0826

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0826;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Пять победителей
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[j + 1]) {
+                    int t = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = t;
+                }
+            }
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task08.task0827;
+
+import java.util.Date;
+
+/* 
+Работа с датой
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isDateOdd("MAY 1 2013"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        return true;
+    }
+}
