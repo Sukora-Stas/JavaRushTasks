2c06539c3d66b87b1c486cada9f3bdc205880a3b
Sukora-Stas <stasiksukora@mail.ru>
2017-06-28 23:16:24 +0300

    task0920

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0920/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task09.task0920;
+
+/* 
+Обратный отсчёт
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //напишите тут ваш код
+        }
+    }
+}
