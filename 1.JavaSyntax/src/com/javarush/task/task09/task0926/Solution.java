97be9231878d8e02340c9694465c25b35254495f
Sukora-Stas <stasiksukora@mail.ru>
2017-06-29 00:20:47 +0300

    task0926

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task09.task0926;
+
+import java.util.ArrayList;
+
+/* 
+Список из массивов чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<int[]> list = createList();
+        printList(list);
+    }
+
+    public static ArrayList<int[]> createList() {
+        //напишите тут ваш код
+        ArrayList<int[]> list = new ArrayList<>();
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i;
+        }
+        list.add(arr);
+
+        int[] arr1 = new int[2];
+        for (int i = 0; i < 2; i++) {
+            arr1[i] = i;
+        }
+        list.add(arr1);
+
+        int[] arr2 = new int[4];
+        for (int i = 0; i < 4; i++) {
+            arr2[i] = i;
+        }
+        list.add(arr2);
+
+        int[] arr3 = new int[7];
+        for (int i = 0; i < 7; i++) {
+            arr3[i] = i;
+        }
+        list.add(arr3);
+
+        int[] arr4 = new int[0];
+
+        list.add(arr4);
+
+        return list;
+    }
+
+    public static void printList(ArrayList<int[]> list) {
+        for (int[] array : list) {
+            for (int x : array) {
+                System.out.println(x);
+            }
+        }
+    }
+}
