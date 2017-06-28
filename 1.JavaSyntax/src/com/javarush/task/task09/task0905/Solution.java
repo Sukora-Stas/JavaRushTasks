41c36115b3d86d8589d6ae2557e12e0d8fdac922
Sukora-Stas <stasiksukora@mail.ru>
2017-06-28 19:44:23 +0300

    task0905

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0905/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task09.task0905;
+
+/* 
+Там, в синих глубинах стек-трейса…
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int deep = getStackTraceDeep();
+    }
+
+    public static int getStackTraceDeep() {
+        //напишите тут ваш код
+        System.out.println(Thread.currentThread().getStackTrace().length);
+        return Thread.currentThread().getStackTrace().length;
+    }
+}
+
