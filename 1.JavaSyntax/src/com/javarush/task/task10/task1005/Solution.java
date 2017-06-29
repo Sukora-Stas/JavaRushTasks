feeec217d8730c9f2a1324fd67e6129981991800
Sukora-Stas <stasiksukora@mail.ru>
2017-06-30 00:10:58 +0300

    task1005

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1005/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task10.task1005;
+
+/* 
+Задача №5 на преобразование целых типов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 44;
+        int b = 300;
+        short c = (short) (b - a);
+        System.out.println(c);
+    }
+}
