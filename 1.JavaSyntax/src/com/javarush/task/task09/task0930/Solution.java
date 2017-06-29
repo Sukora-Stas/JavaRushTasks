588e3efec8420a955415fe912f61a7723f7c15f3
Sukora-Stas <stasiksukora@mail.ru>
2017-06-29 12:26:14 +0300

    task0930

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0930/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task09.task0930;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+        while (true) {
+            String s = reader.readLine();
+            if (s.isEmpty()) break;
+            list.add(s);
+        }
+
+        String[] array = list.toArray(new String[list.size()]);
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (isNumber(array[i]) && isNumber(array[j])) {
+                    if (Integer.parseInt(array[i]) < Integer.parseInt(array[j])) {
+                        String tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+                if (isNumber(array[i]) == false && isNumber(array[j]) == false) {
+                    if (isGreaterThan(array[i], array[j])) {
+                        String tmp = array[j];
+                        array[j] = array[i];
+                        array[i] = tmp;
+                    }
+                }
+            }
+        }
+    }
+
+    // Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+
+
+    // Переданная строка - это число?
+    public static boolean isNumber(String s) {
+        if (s.length() == 0) return false;
+
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if ((i != 0 && c == '-') // есть '-' внутри строки
+                    || (!Character.isDigit(c) && c != '-')) // не цифра и не начинается с '-'
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
