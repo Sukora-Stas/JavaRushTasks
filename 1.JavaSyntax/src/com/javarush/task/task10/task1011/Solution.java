e0e454ac0fe5d9b1b8684828f9809ff4c6d33c9d
Sukora-Stas <stasiksukora@mail.ru>
2017-06-30 19:01:18 +0300

    task1011

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1011/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task10.task1011;
+
+/* 
+Большая зарплата
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Я не хочу изучать Java, я хочу большую зарплату";
+
+        for (int i = 0; i < 40; i++) {
+            System.out.println(s.substring(i).trim());
+        }
+        //напишите тут ваш код
+    }
+
+}
+
