3eca5e50a74571b98f4dac172a223b3f935454bf
Sukora-Stas <stasiksukora@mail.ru>
2017-07-04 23:49:20 +0300

    task1122

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1122/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task11.task1122;
+
+/* 
+Помощь шахматной школе
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+    public class ChessFigure{
+
+    }
+
+    public class King extends ChessFigure{
+    }
+
+    public class Queen extends ChessFigure{
+    }
+
+    public class Rook extends ChessFigure{
+    }
+
+    public class Knight extends ChessFigure{
+    }
+
+    public class Bishop extends ChessFigure{
+    }
+
+    public class Pawn extends ChessFigure{
+    }
+}
