cd6686db3f7464909dd7af4344d4c1253ed60049
Sukora-Stas <stasiksukora@mail.ru>
2017-07-06 00:04:44 +0300

    Task --> 1304

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1304/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task13.task1304;
+
+/* 
+Selectable и Updatable
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    public static class Screen implements Selectable, Updatable {
+
+        @Override
+        public void onSelect() {
+
+        }
+
+        @Override
+        public void refresh() {
+
+        }
+    }
+    //напишите тут ваш класс
+}
