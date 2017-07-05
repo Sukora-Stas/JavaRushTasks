243a053259301380729693ede2e924b31d386397
Sukora-Stas <stasiksukora@mail.ru>
2017-07-06 00:03:12 +0300

    Task --> 1229

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1229/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1229;
+
+/* 
+Родитель класса CTO
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static class CTO extends Primogenitor implements Businessman {
+
+    }
+
+    public static class Primogenitor implements Businessman {
+        @Override
+        public void workHard() {
+
+        }
+    }
+}
