6b57c999f8624770122c33410778a871cf090ecb
Sukora-Stas <stasiksukora@mail.ru>
2017-07-05 14:26:11 +0300

    task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(10);
+        print((Integer) 20);
+    }
+
+    public static void print(int a) {
+        System.out.println(a);
+    }
+
+    public static void print(Integer a) {
+        System.out.println(a);
+
+    }
+
+    //Напишите тут ваши методы
+}
