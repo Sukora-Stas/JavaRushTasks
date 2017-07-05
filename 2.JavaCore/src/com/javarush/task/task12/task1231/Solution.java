03348e0178c65b40a2e48ae372b0c025ba08a95e
Sukora-Stas <stasiksukora@mail.ru>
2017-07-06 00:03:36 +0300

    Task --> 1231

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1231/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task12.task1231;
+
+/* 
+Ненужные абстракции
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Horse horse = new Pegas();
+        horse.run();
+
+        Horse horse1 = new Horse();
+        horse1.run();
+    }
+
+    public static interface Fly {
+        public abstract void fly();
+    }
+
+    public static  class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegas extends Horse implements Fly {
+        public  void fly() {
+
+        }
+    }
+
+    public static abstract class SwimPegas extends Pegas {
+        public abstract void swim();
+    }
+
+}
