c8174d1df1181ce8e18ab96e67b970a650bd280d
Sukora-Stas <stasiksukora@mail.ru>
2017-07-06 00:05:40 +0300

    Task --> 1309

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task13.task1309;
+
+/* 
+Всё, что движется
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Movable {
+        Double speed();
+    }
+
+    interface Flyable extends Movable {
+        Double speed(Flyable flyable);
+    }
+}
