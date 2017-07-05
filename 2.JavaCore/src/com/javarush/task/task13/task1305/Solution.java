134cc2fe8e99dbacec7b00fd44a2fb7a17a9a296
Sukora-Stas <stasiksukora@mail.ru>
2017-07-06 00:04:53 +0300

    Task --> 1305

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1305/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task13.task1305;
+
+/* 
+Четыре ошибки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(Dream.HOBBIE.toString());
+        System.out.println(new Hobbie().toString());
+
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        static Hobbie HOBBIE = new Hobbie();
+    }
+
+    static class Hobbie implements Dream, Desire {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+
+}
