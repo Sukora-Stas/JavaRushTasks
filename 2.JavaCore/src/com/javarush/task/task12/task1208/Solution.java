6556e2df49c3bf069e4b335998f87f7271f44091
Sukora-Stas <stasiksukora@mail.ru>
2017-07-05 14:33:34 +0300

    task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print() {
+
+    }
+
+    public static void print(String s) {
+
+    }
+
+    public static void print(String s, int a) {
+
+    }
+
+    public static void print(String s, String s2) {
+
+    }
+
+    public static void print(String s, int a, String sw) {
+
+    }
+    //Напишите тут ваши методы
+}
