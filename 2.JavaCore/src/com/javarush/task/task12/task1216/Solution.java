10ac2555a56de463f4c67762da5b22dcd8296e7b
Sukora-Stas <stasiksukora@mail.ru>
2017-07-05 18:34:03 +0300

    Task --> 1216

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add an interface here - добавь интерфейс тут
+
+    public interface Fly {
+        void goFly();
+
+        void goInEarth();
+    }
+
+}
