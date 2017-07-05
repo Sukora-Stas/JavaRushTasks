e61e9c1234b6c788d63c28860aba0fa6fb11fddb
Sukora-Stas <stasiksukora@mail.ru>
2017-07-06 00:02:23 +0300

    Task --> 1225

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1225/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task12.task1225;
+
+/* 
+Посетители
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Cow()));
+        System.out.println(getObjectType(new Animal()));
+    }
+
+    public static String getObjectType(Object o) {
+        //напишите тут ваш код
+        return
+                (o instanceof Tiger) ? "Тигр" :
+                        (o instanceof Lion) ? "Лев" :
+                                (o instanceof Cat) ? "Кот" :
+                                        (o instanceof Bull) ? "Бык" :
+                                                (o instanceof Cow) ? "Корова" :
+                                                        "Животное";
+    }
+
+    public static class Cat extends Animal   //<--Классы наследуются!!
+    {
+    }
+
+    public static class Tiger extends Cat {
+    }
+
+    public static class Lion extends Cat {
+    }
+
+    public static class Bull extends Animal {
+    }
+
+    public static class Cow extends Animal {
+    }
+
+    public static class Animal {
+    }
+}
