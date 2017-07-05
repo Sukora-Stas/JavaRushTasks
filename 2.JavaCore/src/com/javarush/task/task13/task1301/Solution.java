9b378f34c632ca65671c999b6b65b8a8e032ecc2
Sukora-Stas <stasiksukora@mail.ru>
2017-07-06 00:04:18 +0300

    Task --> 1301

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1301/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task13.task1301;
+
+/* 
+Пиво
+*/
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Drink {
+        void askMore(String message);
+
+        void sayThankYou();
+
+        boolean isReadyToGoHome();
+    }
+
+    public interface Alcohol extends Drink {
+        boolean READY_TO_GO_HOME = false;
+
+        void sleepOnTheFloor();
+    }
+
+    public static class Beer implements Alcohol {
+
+        @Override
+        public void askMore(String message) {
+
+        }
+
+        @Override
+        public void sayThankYou() {
+
+        }
+
+        @Override
+        public boolean isReadyToGoHome() {
+            return READY_TO_GO_HOME;
+        }
+
+        @Override
+        public void sleepOnTheFloor() {
+
+        }
+    }
+}
