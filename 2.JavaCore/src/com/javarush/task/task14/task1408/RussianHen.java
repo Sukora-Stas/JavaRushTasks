ebbf3334fd772b2bc6d978fa2238bb771e22794c
Sukora-Stas <stasiksukora@mail.ru>
2017-07-12 00:05:28 +0300

    Task --> 1401

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/RussianHen.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task14.task1408;
+
+import com.javarush.task.task14.task1408.Solution;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class RussianHen extends Hen {
+    @Override
+    public int getCountOfEggsPerMonth() {
+        return 0;
+    }
+}
