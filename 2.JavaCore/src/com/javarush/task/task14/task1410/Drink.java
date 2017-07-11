c149680674b1bb48a469c2dedacec8cd0a43e9fe
Sukora-Stas <stasiksukora@mail.ru>
2017-07-12 01:40:24 +0300

    Task --> 1410

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Drink.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task14.task1410;
+
+/**
+ * Created by Sukora Stas.
+ */
+public abstract class Drink {
+    public void taste(){
+        System.out.println("Вкусно");
+    }
+}
