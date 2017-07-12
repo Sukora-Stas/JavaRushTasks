89dc9fa37b35674b99c22aea8d3ec5fc05332598
Sukora-Stas <stasiksukora@mail.ru>
2017-07-12 11:08:31 +0300

    Task --> 1413

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/CompItem.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task14.task1413;
+
+/**
+ * Created by Sukora Stas.
+ */
+public interface CompItem {
+    String getName();
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Monitor.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1413;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Monitor implements CompItem {
+    @Override
+    public String getName() {
+        return "";
+    }
+}
