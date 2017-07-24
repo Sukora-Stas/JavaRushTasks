65769cb3440718a1d990780fb92987d8128cd620
Sukora-Stas <stasiksukora@mail.ru>
2017-07-25 00:17:18 +0300

    Task --> 1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution() {
+    }
+
+    private Solution(String name) {
+
+    }
+
+    Solution(Integer integer) {
+
+    }
+
+    protected Solution(Short s) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+
+    SubSolution(Integer integer) {
+        super(integer);
+    }
+
+    protected SubSolution(Short s) {
+        super(s);
+    }
+}
