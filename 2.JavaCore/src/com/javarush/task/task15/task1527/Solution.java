3a0d16c49f44b4d6d7f213daf26466aefa54ebda
Sukora-Stas <stasiksukora@mail.ru>
2017-07-25 20:47:22 +0300

    Task --> 1527

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -9,8 +9,29 @@ import java.io.InputStreamReader;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //add your code here
+        //TODO: wtf this working?
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String url = bufferedReader.readLine();
+        String [] urlParamsTemp = url.split("\\?");
+        String [] urlParams = urlParamsTemp[1].split("\\&");
+        String[] ahtung = null;
+        for (String s: urlParams) {
+            String[] strTemp = s.split("=");
+            if ("obj".equals(strTemp[0]))
+                ahtung = strTemp;
+            System.out.print(strTemp[0] + " ");
+
+        }
+        System.out.println();
+        if (ahtung != null)
+            try {
+                alert(Double.parseDouble(ahtung[1]));
+            }
+            catch (NumberFormatException e){
+                alert(ahtung[1]);
+            }
     }
 
     public static void alert(double value) {
