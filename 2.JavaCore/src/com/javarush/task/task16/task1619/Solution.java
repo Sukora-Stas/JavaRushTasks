ea13dcff6de4109320a8cfee1079f51b13a97764
Sukora-Stas <stasiksukora@mail.ru>
2017-07-26 20:33:04 +0200

    Task --> 1619

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+*/
+
+public class Solution {
+    static boolean isCancel;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() throws InterruptedException {
+        isCancel = true;
+    }
+
+    public static class TestThread implements Runnable {
+        public void run() {
+            while (!isCancel) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
