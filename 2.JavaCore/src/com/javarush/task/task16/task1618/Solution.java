61adcae3a25551ecc72aa0324d848b95a9830360
Sukora-Stas <stasiksukora@mail.ru>
2017-07-26 20:28:17 +0200

    Task --> 1618

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1618/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task16.task1618;
+
+/* 
+Снова interrupt
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        //Add your code here - добавь код тут
+        TestThread thread =new TestThread();
+        thread.start();
+        thread.interrupt();
+    }
+
+    //Add your code below - добавь код ниже
+    public static class TestThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+}
