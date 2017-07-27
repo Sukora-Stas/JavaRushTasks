9fdbb2cdbb5435c434b1d3cf9cf479fc505ddf06
Sukora-Stas <stasiksukora@mail.ru>
2017-07-27 15:20:54 +0200

    Task --> 1621

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1621/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task16.task1621;
+
+/* 
+Thread.currentThread - всегда возвращает текущую нить
+*/
+
+public class Solution {
+    static int count = 5;
+
+    public static void main(String[] args) {
+        NameOfDifferentThreads tt = new NameOfDifferentThreads();
+        tt.start();
+        for (int i = 0; i < count; i++) {
+            tt.printMsg();
+        }
+    }
+
+    public static class NameOfDifferentThreads extends Thread {
+        public void run() {
+            for (int i = 0; i < count; i++) {
+                printMsg();
+            }
+        }
+
+        public void printMsg() {
+            Thread t = Thread.currentThread();//присвой переменной t текущую нить
+            String name = t.getName();
+            System.out.println("name=" + name);
+            //add sleep here - добавь sleep тут
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
