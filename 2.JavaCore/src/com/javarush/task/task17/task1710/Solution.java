dcdb2f29ab8037aea39f5121da8a3cded930d437
Sukora-Stas <stasiksukora@mail.ru>
2017-07-31 11:20:09 +0200

    Task --> 1710

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,77 +1,61 @@
 package com.javarush.task.task17.task1710;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.javarush.task.task17.task1710.*;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-
 /* 
 CRUD
 */
-
 public class Solution {
     public static List<Person> allPeople = new ArrayList<Person>();
-
     static {
         allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
-
-    public static void main(String[] args) throws Exception {
-        Person person = null;
-        if(args[0].equals("-c"))
-        {
-            if(args[2].equals("м"))
-            {
-                allPeople.add(Person.createMale(args[1],new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(args[3])));
-            }
-            else if(args[2].equals("ж"))
-            {
-                allPeople.add(Person.createFemale(args[1], new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(args[3])));
-            }
-            System.out.println(allPeople.size()-1);
+    public static void main(String[] args) throws ParseException {
+        String action = args[0];
+        switch (action) {
+            case "-c":
+                create(args[1], args[2], args[3]);
+                break;
+            case "-u":
+                update(args[1], args[2], args[3], args[4]);
+                break;
+            case "-d":
+                delete(args[1]);
+                break;
+            case "-i":
+                show(args[1]);
+                break;
+            default:
         }
-        else if(args[0].equals("-u"))
-        {
-            person =  allPeople.get(Integer.parseInt(args[1]));
-            person.setName(args[2]);
-            person.setBirthDay(new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(args[4]));
-            if(args[2].equals("м"))
-            {
-                person.setSex(Sex.MALE);
-            }
-            else if(args[2].equals("ж"))
-            {
-                person.setSex(Sex.FEMALE);
-            }
-            allPeople.set(Integer.parseInt(args[1]), person);
-        }
-        else if(args[0].equals("-d"))
-        {   person = allPeople.get(Integer.parseInt(args[1]));
-            person.setName(null);
-            person.setBirthDay(null);
-            person.setSex(null);
-            allPeople.set(Integer.parseInt(args[1]),person);
-        }
-        else if(args[0].equals("-i"))
-        {
-            person = allPeople.get(Integer.parseInt(args[1]));
-            String sex = "";
-            if(person.getSex().equals(Sex.FEMALE))
-            {
-                sex = "ж";
-            }else if(person.getSex().equals(Sex.MALE))
-            {
-                sex = "м";
-            }
-            SimpleDateFormat simpleDateFormat =  new SimpleDateFormat("dd-MMM-YYYY", Locale.ENGLISH);
-            String date = simpleDateFormat.format(person.getBirthDay());
-            System.out.println(person.getName() + " " + sex + " " + date);
-        }
-
+    }
+    private static void show(String id) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        Person person = allPeople.get(Integer.parseInt(id));
+        System.out.println(person.getName() + " " + (person.getSex() == Sex.MALE ? "м" : "ж") + " " + dateFormat.format(person.getBirthDay()));
+    }
+    private static void delete(String id) {
+        Person person = allPeople.get(Integer.parseInt(id));
+        person.setName(null);
+        person.setBirthDay(null);
+        person.setSex(null);
+    }
+    private static void update(String id, String name, String sex, String bd) throws ParseException {
+        Date birth = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(bd);
+        Person person = allPeople.get(Integer.parseInt(id));
+        person.setName(name);
+        person.setSex("м".equals(sex) ? Sex.MALE : Sex.FEMALE);
+        person.setBirthDay(birth);
+    }
+    private static void create(String name, String sex, String bd) throws ParseException {
+        Date birth = new SimpleDateFormat("dd/MM/yyyy").parse(bd);
+        allPeople.add("м".equals(sex) ? Person.createMale(name, birth) : Person.createFemale(name, birth));
+        System.out.println(allPeople.size() - 1);
     }
 }
