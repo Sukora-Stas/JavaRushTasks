a9989013cc4d06da464b5f8a4af1047ad2f98336
Sukora-Stas <stasiksukora@mail.ru>
2017-08-06 15:08:03 +0200

    Task --> 1812

--- 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -1,12 +1,59 @@
 package com.javarush.task.task18.task1812;
 
-import java.io.*;
-
-/* 
+/*
 Расширяем AmigoOutputStream
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class QuestionFileOutputStream implements AmigoOutputStream {
 
+
+    AmigoOutputStream amigoOutputStream;
+
+
+    public QuestionFileOutputStream(AmigoOutputStream amigoOutputStream) {
+        this.amigoOutputStream = amigoOutputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = reader.readLine();
+
+
+        if (s.equals("Д")) {
+            amigoOutputStream.close();
+
+        }
+    }
+
+
+//
 }
 
