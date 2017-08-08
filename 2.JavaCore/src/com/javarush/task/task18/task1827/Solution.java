b93c3454ba50d976b0c99c826a228e2682b77869
Sukora-Stas <stasiksukora@mail.ru>
2017-08-08 19:26:23 +0200

    Task --> 1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -4,7 +4,75 @@ package com.javarush.task.task18.task1827;
 Прайсы
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        if (args.length < 4 || !args[0].equals("-c")) return;
+        float price;
+        int qty;
+
+        //Если что-то передали не то
+        try {
+            price = Float.parseFloat(args[args.length - 2]);
+            qty = Integer.parseInt(args[args.length - 1]);
+        } catch (NumberFormatException e) {
+            return;
+        }
+
+        //Если у нас аргументов больше 4 из-за пробелов в строке productName
+        String productName;
+        if (args.length > 4) {
+            StringBuffer buf = new StringBuffer();
+            for (int i = 1; i < args.length - 2; i++)
+                buf.append(args[i]).append(" ");
+            productName = buf.substring(0, buf.length() - 1);
+        } else
+            productName = args[1];
+
+        //Read file name from console
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = consoleReader.readLine();
+        consoleReader.close();
+
+        //Get Lines from file
+        //List<String> lines = Files.readAllLines(Paths.get(fileName)); //так проще, но не пропускается
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))) {
+            while (fileReader.ready())
+                lines.add(fileReader.readLine());
+        }
+
+        //Get ID from line
+        Pattern p = Pattern.compile("([0-9]{1,8})");
+        int maxID = 0;
+        for (String s : lines) {
+            Matcher m = p.matcher(s);
+            if (m.lookingAt()) {
+                try {
+                    //System.out.println(s.substring(m.start(), m.end()));
+                    int id = Integer.parseInt(s.substring(m.start(), m.end()));
+                    if (id > maxID)
+                        maxID = id;
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+            }
+        }
+
+        if (maxID++ == 99999999)
+            return;
+        String toFile = String.format(Locale.ROOT,"%-8d%-30s%-8.2f%-4d", maxID, productName, price, qty);
+
+        lines.add(toFile);
+        try (BufferedWriter buf = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName)))) {
+            for (String s: lines)
+                buf.write(s+"\r\n");
+        }
     }
 }
