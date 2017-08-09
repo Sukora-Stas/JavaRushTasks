1162552923a29c454727bee234947292a3362995
Sukora-Stas <stasiksukora@mail.ru>
2017-08-09 12:47:37 +0200

    Task --> 1907

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task19.task1907;
+
+/* 
+Считаем слово
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader conReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = conReader.readLine();
+        conReader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+        int count = 0;
+        String word = "world";
+        while (fileReader.ready()) {
+            String line = fileReader.readLine();
+            String[] words = line.toString().split("\\W");
+            for (String s : words)
+                if (s.equals(word))
+                    count++;
+        }
+        fileReader.close();
+
+        System.out.println(count);
+    }
+}
