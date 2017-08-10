0953fa59db35514ae49175dead3c157b9cfaa864
Sukora-Stas <stasiksukora@mail.ru>
2017-08-10 09:57:21 +0200

    Task --> 1923

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task19.task1923;
+
+/* 
+Слова с цифрами
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(args[0]));
+        BufferedWriter printWriter = new BufferedWriter(new FileWriter(args[1]));
+
+        while (fileReader.ready()) {
+            String line = fileReader.readLine();
+            String[] words = line.split(" ");
+            for (String word : words)
+                if (!word.matches("^\\D*$"))
+                    printWriter.write(word + " ");
+        }
+        fileReader.close();
+        printWriter.close();
+    }
+}
