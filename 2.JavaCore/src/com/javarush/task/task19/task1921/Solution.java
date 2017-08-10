829da490f2852ddf544a043ee88feefbc90111d7
Sukora-Stas <stasiksukora@mail.ru>
2017-08-10 09:44:40 +0200

    Task --> 1921

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1921;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/* 
+Хуан Хуанович
+*/
+
+public class Solution {
+    public static final List<Person> PEOPLE = new ArrayList<Person>();
+
+    public static void main(String[] args) throws IOException {
+        String fileName = args[0];
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            while (fileReader.ready()) {
+                String line = fileReader.readLine();
+                String nameStr = line.replaceAll("[\\w]", "").trim();
+                String[] digitalParts = line.substring(nameStr.length()).trim().split(" ");
+                int day = Integer.parseInt(digitalParts[0]);
+                int month = Integer.parseInt(digitalParts[1]);
+                int year = Integer.parseInt(digitalParts[2]);
+                Date date = new GregorianCalendar(year, month - 1, day).getTime();
+
+                PEOPLE.add(new Person(nameStr, date));
+            }
+        }
+
+    }
+}
