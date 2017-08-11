2f7c7c8571d0504c82a84f48b153e6033933c2b5
Sukora-Stas <stasiksukora@mail.ru>
2017-08-12 00:02:06 +0200

    Task --> 2017

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2017/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task20.task2017;
+
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/* 
+Десериализация
+*/
+public class Solution {
+    public A getOriginalObject(ObjectInputStream objectStream) {
+        A a = null;
+        try {
+            a = (A) objectStream.readObject();
+        } catch (NotSerializableException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return a;
+    }
+
+    public class A implements Serializable {
+    }
+
+    public class B extends A {
+        public B() {
+            System.out.println("inside B");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
