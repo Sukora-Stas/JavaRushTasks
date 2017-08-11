700c26d2ff06e6e141e1d29c07dab8be79cacd6c
Sukora-Stas <stasiksukora@mail.ru>
2017-08-11 12:56:56 +0200

    Task --> 2009

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task20.task2009;
+
+import java.io.Serializable;
+
+/*
+Как сериализовать static?
+*/
+public class Solution {
+    public static class ClassWithStatic implements Serializable {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
