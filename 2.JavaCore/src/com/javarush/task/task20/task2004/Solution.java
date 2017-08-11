d341d308ae961351bef54202aa63c2c2b51a1db9
Sukora-Stas <stasiksukora@mail.ru>
2017-08-11 12:46:25 +0200

    Task --> 2004

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2004/Solution.java
@@ -0,0 +1,102 @@
+package com.javarush.task.task20.task2004;
+
+import java.io.*;
+
+/* 
+Читаем и пишем в файл статики
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //you can find your_file_name.tmp in your TMP directory or fix outputStream/inputStream according to your real file location
+        //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+
+            File your_file_name = File.createTempFile("your_file_name", null);
+            OutputStream outputStream = new FileOutputStream(your_file_name);
+            InputStream inputStream = new FileInputStream(your_file_name);
+
+            ClassWithStatic classWithStatic = new ClassWithStatic();
+            classWithStatic.i = 3;
+            classWithStatic.j = 4;
+            classWithStatic.save(outputStream);
+            outputStream.flush();
+
+            ClassWithStatic loadedObject = new ClassWithStatic();
+            loadedObject.staticString = "something";
+            loadedObject.i = 6;
+            loadedObject.j = 7;
+
+            loadedObject.load(inputStream);
+            //check here that classWithStatic object equals to loadedObject object - проверьте тут, что classWithStatic и loadedObject равны
+
+            outputStream.close();
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+    public static class ClassWithStatic {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.println(staticString);
+            printWriter.println(i);
+            printWriter.println(j);
+            printWriter.close();
+
+            /*
+            Валидатор не принимает если делаем так:
+            Properties prop = new Properties();
+            prop.put("staticString", ClassWithStatic.staticString);
+            prop.put("i", String.valueOf(this.i));
+            prop.put("j", String.valueOf(this.j));
+            prop.store(outputStream, "");*/
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            ClassWithStatic.staticString = bufferedReader.readLine();
+            this.i = Integer.parseInt(bufferedReader.readLine());
+            this.j = Integer.parseInt(bufferedReader.readLine());
+            bufferedReader.close();
+
+
+            /*Валидатор не принимает если делаем так:
+            Properties prop = new Properties();
+            prop.load(inputStream);
+            ClassWithStatic.staticString = prop.getProperty("taticString");
+            this.i = Integer.parseInt(prop.getProperty("i"));
+            this.j = Integer.parseInt(prop.getProperty("j"));*/
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ClassWithStatic that = (ClassWithStatic) o;
+
+            if (i != that.i) return false;
+            return j == that.j;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = i;
+            result = 31 * result + j;
+            return result;
+        }
+    }
+}
