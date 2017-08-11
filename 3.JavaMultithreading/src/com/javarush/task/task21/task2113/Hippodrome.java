005ab5e2b9ab28a550c0a5b52a369ecef6a01c8c
Sukora-Stas <stasiksukora@mail.ru>
2017-08-11 18:23:36 +0200

    Task --> 2113 bigTask3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.List;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Hippodrome {
+    private List<Horse> horses;
+    public static void main(String[] args) {
+
+    }
+
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Horse {
+}
