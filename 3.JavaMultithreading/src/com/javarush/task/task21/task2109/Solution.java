492dd03eeaec5994d39445a22942c767761b3090
Sukora-Stas <stasiksukora@mail.ru>
2017-08-13 00:53:08 +0200

    Task --> 2109

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+    }
+
+    public static class C extends B implements Cloneable {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected C clone() throws CloneNotSupportedException {
+            int i = getI();
+            int j = getJ();
+            String name = getName();
+
+            return new C(i, j, name);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
