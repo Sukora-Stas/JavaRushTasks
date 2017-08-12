3909c8ef2c92444d560a1f4539a9a45bd3f18486
Sukora-Stas <stasiksukora@mail.ru>
2017-08-13 00:52:05 +0200

    Task --> 2105

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task21.task2105;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Исправить ошибку. Сравнение объектов
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+
+        if (!(o instanceof Solution)) {
+            return false;
+        }
+
+        if (this == o) return true;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (last != null ? last.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}
