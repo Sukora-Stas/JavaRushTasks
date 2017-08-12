cb737e51ef57bb643536e6f7169e0009dee4f825
Sukora-Stas <stasiksukora@mail.ru>
2017-08-13 00:52:42 +0200

    Task --> 2107

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -0,0 +1,73 @@
+package com.javarush.task.task21.task2107;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Глубокое клонирование карты
+*/
+public class Solution implements Cloneable {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.users.put("Hubert", new User(172, "Hubert"));
+        solution.users.put("Zapp", new User(41, "Zapp"));
+        Solution clone = null;
+        try {
+            clone = solution.clone();
+            System.out.println(solution);
+            System.out.println(clone);
+
+            System.out.println(solution.users);
+            System.out.println(clone.users);
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    protected Map<String, User> users = new LinkedHashMap();
+
+    public static class User implements Cloneable {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        protected User clone() throws CloneNotSupportedException {
+            return new User(this.age, this.name);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof User)) return false;
+
+            User user = (User) o;
+
+            if (age != user.age) return false;
+            return name.equals(user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = age;
+            result = 31 * result + name.hashCode();
+            return result;
+        }
+    }
+
+    @Override
+    protected Solution clone() throws CloneNotSupportedException {
+        Solution solution = new Solution();
+        for (Map.Entry<String, User> pair : this.users.entrySet()) {
+            User user = pair.getValue().clone();
+            solution.users.put(pair.getKey(), user);
+        }
+        return solution;
+    }
+}
