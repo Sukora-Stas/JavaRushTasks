0a38568751a061e02ab7a1fb964709d2587621b5
SukoraStas <stasiksukora@mail.ru>
2017-08-22 21:53:26 +0300

    Task --> 2312 Snake -- 7

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,7 +1,29 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.List;
+
 /**
  * Created by Sukora Stas.
  */
 public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public List<SnakeSection> getSections() {
+
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
 }
