718d9c1783e589cd76c4ac10ec10397790c0a668
SukoraStas <stasiksukora@mail.ru>
2017-08-22 21:49:11 +0300

    Task --> 2312 Snake -- 6

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -4,4 +4,19 @@ package com.javarush.task.task23.task2312;
  * Created by Sukora Stas.
  */
 public class SnakeSection {
+    private int x;
+    private int y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
