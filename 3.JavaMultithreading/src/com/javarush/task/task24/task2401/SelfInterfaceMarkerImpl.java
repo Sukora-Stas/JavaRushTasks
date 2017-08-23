240d0ac42fd39249cfd4818960507ed7fc7313f7
SukoraStas <stasiksukora@mail.ru>
2017-08-23 18:10:41 +0300

    Task --> 2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+    public void shoMeColor () {
+        System.out.println("Green");
+    }
+
+    public void songMe () {
+        System.out.println("La La La");
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/UnsupportedInterfaceMarkerException.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class UnsupportedInterfaceMarkerException extends Exception {
+}
