05edbbae692632d3fedcf3294d32192d436f52b1
SukoraStas <stasiksukora@mail.ru>
2017-08-23 13:55:44 +0300

    Task --> 2301

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2301/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task23.task2301;
+
+/* 
+Запрети наследование
+*/
+public class Solution {
+
+    public static final class Listener {
+        public void onMouseDown(int x, int y) {
+            //do something on mouse down event
+        }
+
+        public void onMouseUp(int x, int y) {
+            //do something on mouse up event
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
