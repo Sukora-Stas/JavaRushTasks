338526a65ef6ecae1901137d53f7a28441dcd54e
SukoraStas <stasiksukora@mail.ru>
2017-08-24 20:46:40 +0300

    Task --> 2413 Arkanoid -- 16

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -4,6 +4,31 @@ package com.javarush.task.task24.task2413;
  * Created by Sukora Stas.
  */
 public class Stand extends BaseObject {
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
     }
