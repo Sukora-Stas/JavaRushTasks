f9a8d217420850c98c9124bf7cb63e893095f593
SukoraStas <stasiksukora@mail.ru>
2017-08-24 19:27:32 +0300

    Task --> 2413 Arkanoid -- 10

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -14,6 +14,30 @@ public class Canvas {
         matrix = new char[height + 2][width + 2];
     }
 
+    public void setPoint(double x, double y, char c) {
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+        if (y0 < 0 || y0 >= matrix.length) return;
+        if (x0 < 0 || x0 >= matrix[y0].length) return;
+
+        matrix[y0][x0] = c;
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (matrix[i][j] == 1)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
+
+
     public int getWidth() {
         return width;
     }
