b96ccbf5479499a3308f430418256b9e7a868e75
SukoraStas <stasiksukora@mail.ru>
2017-08-24 16:07:35 +0300

    Task --> 2413 Arkanoid -- 2

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -4,7 +4,26 @@ package com.javarush.task.task24.task2413;
  * Created by Sukora Stas.
  */
 public class Arkanoid {
+    private int width;
+    private int height;
+
     public static void main(String[] args) {
 
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
