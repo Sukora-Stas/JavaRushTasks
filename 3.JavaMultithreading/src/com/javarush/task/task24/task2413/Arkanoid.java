2b491f6baa5913da75e2d3badb150b7a579b6844
SukoraStas <stasiksukora@mail.ru>
2017-08-24 16:12:14 +0300

    Task --> 2413 Arkanoid -- 4

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,11 +1,17 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 /**
  * Created by Sukora Stas.
  */
 public class Arkanoid {
     private int width;
     private int height;
+
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
 
     public static void main(String[] args) {
 
@@ -30,5 +36,29 @@ public class Arkanoid {
 
     public void setHeight(int height) {
         this.height = height;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 }
