4d9c9bd54dd7ef7793395bbd3cdeda64fe7f87f5
SukoraStas <stasiksukora@mail.ru>
2017-08-24 16:23:38 +0300

    Task --> 2413 Arkanoid -- 7

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -4,4 +4,7 @@ package com.javarush.task.task24.task2413;
  * Created by Sukora Stas.
  */
 public class Brick extends BaseObject {
+    public Brick(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -4,4 +4,7 @@ package com.javarush.task.task24.task2413;
  * Created by Sukora Stas.
  */
 public class Stand extends BaseObject {
+    public Stand(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
