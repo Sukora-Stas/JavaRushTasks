0f2b829fa64358fe94764a888d1369daf357db9e
SukoraStas <stasiksukora@mail.ru>
2017-08-25 13:25:28 +0300

    Task --> 2413 MVC -- 1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("a", 1, 1));
+        users.add(new User("b", 1, 1));
+
+        modelData.setUsers(users);
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task36.task3608.model;
+
+/**
+ * Created by Sukora Stas.
+ */
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+}
