cdad40f822f042a61713fd7b58cf351219609de4
SukoraStas <stasiksukora@mail.ru>
2017-08-25 14:40:28 +0300

    Task --> 2413 MVC -- 4

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+}
