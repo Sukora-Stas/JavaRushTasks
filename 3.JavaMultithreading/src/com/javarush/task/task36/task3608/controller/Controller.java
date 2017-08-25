913fae285b73944bf02f58bbff225b40b2218fea
SukoraStas <stasiksukora@mail.ru>
2017-08-25 13:51:39 +0300

    Task --> 2413 MVC -- 3

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,18 +1,28 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 /**
  * Created by Sukora Stas.
  */
 public class Controller {
     private Model model;
+    private UsersView usersView;
 
     public void onShowAllUsers(){
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+
     }
 
     public void setModel(Model model) {
         this.model = model;
     }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+
+
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+import java.util.List;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class UsersView implements View {
+    private Controller controller;
+
+    public void fireEventShowAllUsers() {
+    controller.onShowAllUsers();
+    }
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        for (int i = 0; i < modelData.getUsers().size(); i++) {
+            System.out.println("\t" + modelData.getUsers().get(i));
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+
+    }
+
+}
