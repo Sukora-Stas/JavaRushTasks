c91d9e6aff46733a3e66cd21f17c28e8ee1ff806
SukoraStas <stasiksukora@mail.ru>
2017-08-30 17:01:47 +0300

    Task --> 3608 Chat -- 8

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -14,7 +14,12 @@ public class Server {
     // ключ -имя клиента, а значение - соединение с ним
     private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) {
+
+    /**
+     * MAIN
+     **/
+    public static void main(String[] args) throws IOException {
+
         ConsoleHelper.writeMessage("Введите порт сервера: ");
         int serverPort = ConsoleHelper.readInt();
 
@@ -29,9 +34,8 @@ public class Server {
                 //запускаем handler
                 handler.start();
             }
-        } catch (IOException e) {
-
         }
+
     }
 
 
@@ -54,7 +58,11 @@ public class Server {
     }
 
 
+    /**
+     * обработчик Handler, в котором будет происходить обмен сообщениями с клиентом
+     **/
     private static class Handler extends Thread {
+
         private Socket socket;
 
         //Constructor
@@ -62,5 +70,111 @@ public class Server {
 
             this.socket = socket;
         }
+
+
+//        @Override
+//        public void run() {
+//
+//            ConsoleHelper.writeMessage("Установленно соединение с адресом " + socket.getRemoteSocketAddress());
+//            String clientName = null;
+//            //Создаем Connection
+//            try (Connection connection = new Connection(socket)) {
+//                //Выводить сообщение, что установлено новое соединение с удаленным адресом
+//                ConsoleHelper.writeMessage("Подключение к порту: " + connection.getRemoteSocketAddress());
+//                //Вызывать метод, реализующий рукопожатие с клиентом, сохраняя имя нового клиента
+//                clientName = serverHandshake(connection);
+//                //Рассылать всем участникам чата информацию об имени присоединившегося участника (сообщение с типом USER_ADDED)
+//                sendBroadcastMessage(new Message(MessageType.USER_ADDED, clientName));
+//                //Сообщать новому участнику о существующих участниках
+//                sendListOfUsers(connection, clientName);
+//                //Запускать главный цикл обработки сообщений сервером
+//                serverMainLoop(connection, clientName);
+//
+//
+//            } catch (IOException e) {
+//                ConsoleHelper.writeMessage("Ошибка при обмене данными с удаленным адресом");
+//            } catch (ClassNotFoundException e) {
+//                ConsoleHelper.writeMessage("Ошибка при обмене данными с удаленным адресом");
+//            }
+//
+//            //После того как все исключения обработаны, удаляем запись из connectionMap
+//            connectionMap.remove(clientName);
+//            //и отправлялем сообщение остальным пользователям
+//            sendBroadcastMessage(new Message(MessageType.USER_REMOVED, clientName));
+//
+//            ConsoleHelper.writeMessage("Соединение с удаленным адресом закрыто");
+//
+//        }
+
+        /**
+         * Handshake
+         **/
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+
+            while (true) {
+                // Сформировать и отправить команду запроса имени пользователя
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                // Получить ответ клиента
+                Message message = connection.receive();
+
+                // Проверить, что получена команда с именем пользователя
+                if (message.getType() == MessageType.USER_NAME) {
+
+                    //Достать из ответа имя, проверить, что оно не пустое
+                    if (!message.getData().isEmpty()) {
+
+                        // и пользователь с таким именем еще не подключен (используй connectionMap)
+                        if (connectionMap.get(message.getData()) == null) {
+
+                            // Добавить нового пользователя и соединение с ним в connectionMap
+                            connectionMap.put(message.getData(), connection);
+                            // Отправить клиенту команду информирующую, что его имя принято
+                            connection.send(new Message(MessageType.NAME_ACCEPTED));
+
+                            // Вернуть принятое имя в качестве возвращаемого значения
+                            return message.getData();
+                        }
+                    }
+                }
+            }
+        }
+
+
+//        /**
+//         * Отправка списка всех пользователей
+//         **/
+//        private void sendListOfUsers(Connection connection, String userName) throws IOException {
+//
+//            for (String key : connectionMap.keySet()) {
+//                Message message = new Message(MessageType.USER_ADDED, key);
+//
+//                if (!key.equals(userName)) {
+//                    connection.send(message);
+//                }
+//            }
+//        }
+//
+//
+//        /**
+//         * Главный цикл обработки сообщений сервером
+//         **/
+//        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+//
+//            while (true) {
+//
+//                Message message = connection.receive();
+//                // Если принятое сообщение – это текст (тип TEXT)
+//                if (message.getType() == MessageType.TEXT) {
+//
+//                    String s = userName + ": " + message.getData();
+//
+//                    Message formattedMessage = new Message(MessageType.TEXT, s);
+//                    sendBroadcastMessage(formattedMessage);
+//                } else {
+//                    ConsoleHelper.writeMessage("Error");
+//                }
+//            }
+//        }
     }
 }
+
