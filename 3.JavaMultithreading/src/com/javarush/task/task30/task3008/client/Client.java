e3038eadd51778b668a64793d7ef8c7811ebe053
SukoraStas <stasiksukora@mail.ru>
2017-08-30 19:58:53 +0300

    Task --> 3608 Chat -- 13

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by Sukora Stas.
@@ -11,5 +16,64 @@ public class Client {
 
     public class SocketThread extends Thread {
 
+    }
+
+    /**
+     * Должен запросить ввод адреса сервера и вернуть введенное значение
+     **/
+    protected String getServerAddress() {
+
+        ConsoleHelper.writeMessage("Введите адрес сервера: ");
+        return ConsoleHelper.readString();
+    }
+
+
+    /**
+     * Должен запрашивать ввод порта сервера и возвращать его
+     **/
+    protected int getServerPort() {
+
+        ConsoleHelper.writeMessage("Введите порт сервера: ");
+        return ConsoleHelper.readInt();
+    }
+
+
+    /**
+     * Должен запрашивать и возвращать имя пользователя
+     **/
+    protected String getUserName() {
+
+        ConsoleHelper.writeMessage("Введите имя пользователя: ");
+        return ConsoleHelper.readString();
+    }
+
+
+    protected boolean shouldSendTextFromConsole() {
+
+        return true;
+    }
+
+
+    /**
+     * должен создавать и возвращать новый объект класса SocketThread
+     **/
+    protected SocketThread getSocketThread() {
+
+        return new SocketThread();
+    }
+
+
+    /**
+     * создает новое текстовое сообщение, используя переданный текст и отправляет его серверу через соединение connection
+     **/
+    protected void sendTextMessage(String text) {
+
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Ошибка отправки");
+            clientConnected = false;
+        }
     }
 }
