394fc370d564415700a41773d852aa7db44e2dc4
SukoraStas <stasiksukora@mail.ru>
2017-08-30 16:41:23 +0300

    Task --> 3608 Chat -- 5

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,7 +1,60 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 /**
  * Created by Sukora Stas.
  */
-public class Connection {
+public class Connection implements Closeable {
+
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+
+    //Constructor
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+            out.flush();
+
+        }
+
+    }
+
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        Message message;
+
+        synchronized (in) {
+            message = (Message)in.readObject();
+            return message;
+        }
+
+    }
+
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
+
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+
 }
