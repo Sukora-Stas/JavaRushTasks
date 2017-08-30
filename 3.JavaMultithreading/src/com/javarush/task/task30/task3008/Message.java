0cda667fd4f0ca2f1e78cbfc9c35d96472107c87
SukoraStas <stasiksukora@mail.ru>
2017-08-30 16:34:51 +0300

    Task --> 3608 Chat -- 4

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
@@ -1,7 +1,32 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.Serializable;
+
 /**
  * Created by Sukora Stas.
  */
-public class Message {
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String data;
+
+    //Constructors
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+
 }
