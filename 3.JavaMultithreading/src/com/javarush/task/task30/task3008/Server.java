a950a5aeafc418a02ae3f1be91c7fd5b43dac973
SukoraStas <stasiksukora@mail.ru>
2017-08-30 16:51:00 +0300

    Task --> 3608 Chat -- 6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,10 +1,41 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Sukora Stas.
  */
 public class Server {
     public static void main(String[] args) {
 
+        ConsoleHelper.writeMessage("Введите порт сервера: ");
+        int serverPort = ConsoleHelper.readInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+
+            ConsoleHelper.writeMessage("Сервер запущен");
+
+            while (true) {
+                //Слушаем
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                //запускаем handler
+                handler.start();
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+
+    private static class Handler extends Thread {
+        private Socket socket;
+        //Constructor
+        public Handler(Socket socket) {
+
+            this.socket = socket;
+        }
     }
 }
