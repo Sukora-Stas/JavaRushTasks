7da0c1a3a6b1c3fee01f16f0e84ce1cd787aa5a3
SukoraStas <stasiksukora@mail.ru>
2017-08-30 13:21:25 +0300

    Task --> 3608 Chat -- 2

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,7 +1,46 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Sukora Stas.
  */
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String message;
+
+        while (true) {
+            try {
+                message = reader.readLine();
+                break;
+
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return message;
+    }
+
+    public static int readInt() {
+        int i;
+
+        while (true) {
+            try {
+                i = Integer.parseInt(readString());
+                break;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return i;
+    }
 }
