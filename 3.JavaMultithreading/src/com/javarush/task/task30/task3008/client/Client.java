e41d9aa006b490498c0181b6eea45e3718f511bd
SukoraStas <stasiksukora@mail.ru>
2017-08-30 19:53:24 +0300

    Task --> 3608 Chat -- 12

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread {
+
+    }
+}
