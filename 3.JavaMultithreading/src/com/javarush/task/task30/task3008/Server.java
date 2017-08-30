14471e5aefa2edc140a44b386617a004c18eebdb
SukoraStas <stasiksukora@mail.ru>
2017-08-30 16:53:18 +0300

    Task --> 3608 Chat -- 7

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,13 +3,18 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Sukora Stas.
  */
 public class Server {
-    public static void main(String[] args) {
 
+    // ключ -имя клиента, а значение - соединение с ним
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера: ");
         int serverPort = ConsoleHelper.readInt();
 
@@ -30,8 +35,28 @@ public class Server {
     }
 
 
+    /**
+     * отправка сообщения для всех
+     **/
+    public static void sendBroadcastMessage(Message message) {
+
+        try {
+
+            for (Connection connection : connectionMap.values()) {
+                connection.send(message);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            ConsoleHelper.writeMessage("Сообщение не отправлено");
+        }
+
+    }
+
+
     private static class Handler extends Thread {
         private Socket socket;
+
         //Constructor
         public Handler(Socket socket) {
 
