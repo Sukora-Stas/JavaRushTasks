c0f72f08d09055f23af59c35e186b19f782ccc3b
SukoraStas <stasiksukora@mail.ru>
2017-08-31 17:43:41 +0300

    Task --> 3110 Archive -- 3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task31.task3110;
+
+/**
+ * Created by Sukora Stas.
+ */
+public enum Operation {
+    CREATE,
+    ADD,
+    REMOVE,
+    EXTRACT,
+    CONTENT,
+    EXIT
+}
