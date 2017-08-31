48156470aab9d439d2aa70760c02b554bd7e1b10
SukoraStas <stasiksukora@mail.ru>
2017-08-31 17:40:44 +0300

    Task --> 3110 Archive -- 2

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by Sukora Stas.
@@ -12,7 +17,23 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
 
-    public void createZip(Path source) throws Exception{
+    public void createZip(Path source) throws Exception {
 
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream inputStream = Files.newInputStream(source)) {
+
+            ZipEntry sourceEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(sourceEntry);
+
+            //Считываем содержимое файла в массив byte
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+
+            //Добавляем содержимое к архиву
+            zipOutputStream.write(buffer);
+            //Закрываем текущую запись для новой записи
+            zipOutputStream.closeEntry();
+
+        }
     }
 }
