0a05fdc1cbd41e4d22934b20fe24ddefae74e344
SukoraStas <stasiksukora@mail.ru>
2017-08-31 18:30:51 +0300

    Task --> 3110 Archive -- 8

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -2,21 +2,46 @@ package com.javarush.task.task31.task3110;
 
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 /**
  * Created by Sukora Stas.
  */
 public class Archiver {
-    public static void main(String[] args) throws Exception {
 
-        ConsoleHelper.writeMessage("Введите полный путь к архиву: ");
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(ConsoleHelper.readString()));
-        ConsoleHelper.writeMessage("Введите путь к архивируемому файлу: ");
-        zipFileManager.createZip(Paths.get(ConsoleHelper.readString()));
+    public static void main(String[] args) {
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        Operation operation;
+
+        try {
+            while (true) {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+                if (operation == Operation.EXIT)
+                    break;
+            }
+        } catch (WrongZipFileException e) {
+            ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+        } catch (Exception message) {
+            ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+        }
+    }
+
+    public static Operation askOperation() throws IOException {
+
+        ConsoleHelper.writeMessage("Выберите операцию:");
+
+        ConsoleHelper.writeMessage(" " + Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(" " + Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(" " + Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(" " + Operation.EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(" " + Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(" " + Operation.EXIT.ordinal() + " - выход");
+
+        return Operation.values()[ConsoleHelper.readInt()];
+
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/WrongZipFileException.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/WrongZipFileException.java
@@ -1,7 +1,4 @@
 package com.javarush.task.task31.task3110.exception;
 
-/**
- * Created by Sukora Stas.
- */
 public class WrongZipFileException extends Exception {
 }
