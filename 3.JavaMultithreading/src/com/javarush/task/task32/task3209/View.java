cf6a0d5a61d233a84f942469725e8b7e28ea8aab
SukoraStas <stasiksukora@mail.ru>
2017-09-01 14:39:13 +0300

    Task --> 3209 HTML Editor  -- 6

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -31,7 +33,25 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
+        //Устанавливать значение "text/html" в качестве типа контента для компонента htmlTextPane
+        htmlTextPane.setContentType("text/html");
 
+        //Создавать новый локальный компонент JScrollPane на базе htmlTextPane
+        //Добавлять вкладку в панель tabbedPane с именем "HTML" и компонентом из предыдущего пункта
+        tabbedPane.addTab("HTML", new JScrollPane(htmlTextPane));
+
+        //Создавать новый локальный компонент JScrollPane на базе plainTextPane
+        //Добавлять еще одну вкладку в tabbedPane с именем "Текст" и компонентом из предыдущего пункта
+        tabbedPane.addTab("Текст", new JScrollPane(plainTextPane));
+
+        //Устанавливать предпочтительный размер панели tabbedPane
+        tabbedPane.setPreferredSize(new Dimension(800, 600));
+
+        //Создавать объект класса TabbedPaneChangeListener и устанавливать его в качестве слушателя изменений в tabbedPane
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+
+        //Добавлять по центру панели контента текущего фрейма нашу панель с вкладками
+        getContentPane().add(tabbedPane,BorderLayout.CENTER);
     }
 
     public void init() {
