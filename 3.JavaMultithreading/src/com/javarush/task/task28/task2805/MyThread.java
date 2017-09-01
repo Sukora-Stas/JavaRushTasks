debb1e5ba60f855ed437020fa8f5c3c9646578e8
SukoraStas <stasiksukora@mail.ru>
2017-09-01 12:34:55 +0300

    Task --> 3209 HTML Editor  -- 1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2805/MyThread.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task28.task2805;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class MyThread extends Thread {
+
+    private static AtomicInteger currentPriority = new AtomicInteger(0);
+
+    public MyThread() {
+        initialize();
+    }
+
+    private void initialize() {
+        currentPriority.incrementAndGet();
+        currentPriority.compareAndSet(11, 1);
+
+        int newPriority = currentPriority.get();
+        if (getThreadGroup() != null) {
+            if (newPriority > getThreadGroup().getMaxPriority()) {
+                newPriority = getThreadGroup().getMaxPriority();
+            }
+        }
+        setPriority(newPriority);
+    }
+
+    public MyThread(Runnable target) {
+        super(target);
+        initialize();
+    }
+
+    public MyThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+        initialize();
+    }
+
+    public MyThread(String name) {
+        super(name);
+        initialize();
+    }
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+        initialize();
+    }
+
+    public MyThread(Runnable target, String name) {
+        super(target, name);
+        initialize();
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+        initialize();
+    }
+
+    public MyThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+        initialize();
+    }
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class View extends JFrame implements ActionListener {
+
+    private Controller controller;
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
