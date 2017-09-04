20078f44610607ea9a03ad9425333ab1b9f67333
SukoraStas <stasiksukora@mail.ru>
2017-09-04 14:55:24 +0300

    Task --> 3209 HTML Editor -- 20

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -22,11 +22,20 @@ public class Controller {
     }
 
     public void init() {
-
+        createNewDocument();
     }
 
     public void createNewDocument() {
-
+        //Выбирать html вкладку у представления
+        view.selectHtmlTab();
+        //Сбрасывать текущий документ
+        resetDocument();
+        //Устанавливать новый заголовок окна
+        view.setTitle("HTML редактор");
+        //Сбрасывать правки в Undo менеджере
+        view.resetUndo();
+        //Обнулить переменную currentFile
+        currentFile = null;
     }
 
     public void openDocument() {
