e9665ae1fe180580d3096ec94a160066811a5946
SukoraStas <stasiksukora@mail.ru>
2017-09-04 12:20:16 +0300

    Task --> 3209 HTML Editor -- 11

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -2,8 +2,12 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,8 +18,16 @@ import java.awt.event.ActionListener;
 public class View extends JFrame implements ActionListener {
 
     private Controller controller;
+
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+
+    //это будет панель с двумя вкладками
     private JTabbedPane tabbedPane = new JTabbedPane();
+    //это будет компонент для визуального редактирования html
     private JTextPane htmlTextPane = new JTextPane();
+    //это будет компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое)
     private JEditorPane plainTextPane = new JEditorPane();
 
     //constructor
@@ -89,13 +101,32 @@ public class View extends JFrame implements ActionListener {
     }
 
     public boolean canUndo() {
-//        return undoManager.canUndo();
-        return false;
+        return undoManager.canUndo();
     }
 
     public boolean canRedo() {
-//        return undoManager.canRedo();
-        return false;
+        return undoManager.canRedo();
+    }
+
+    public void undo() {
+
+        try {
+            undoManager.undo();
+        } catch (CannotUndoException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (CannotRedoException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
     }
 
     public void exit() {
@@ -113,5 +144,9 @@ public class View extends JFrame implements ActionListener {
 
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoListener.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task32.task3209.listeners;
+
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.undo.UndoManager;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class UndoListener implements UndoableEditListener {
+    private UndoManager undoManager;
+
+    public UndoListener(UndoManager undoManager) {
+        this.undoManager = undoManager;
+    }
+
+    @Override
+    public void undoableEditHappened(UndoableEditEvent e) {
+        undoManager.addEdit(e.getEdit());
+
+    }
+}
