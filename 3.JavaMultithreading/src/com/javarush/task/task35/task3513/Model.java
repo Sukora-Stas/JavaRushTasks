3564c77b219fb2efd19599cddfee121eb4fd4b28
SukoraStas <stasiksukora@mail.ru>
2017-09-06 00:55:31 +0300

    Task --> 3513 "2048" -- 3

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -4,4 +4,17 @@ package com.javarush.task.task35.task3513;
  * Created by Sukora Stas.
  */
 public class Model {
+
+    private static final int FIELD_WIDTH = 4;
+
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    public Model(){
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
+
 }
