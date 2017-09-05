7071a18c7145e042984a7ce36663c25701b11c3f
SukoraStas <stasiksukora@mail.ru>
2017-09-06 00:50:08 +0300

    Task --> 3513 "2048" -- 2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Tile.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task35.task3513;
+
+import java.awt.*;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Tile {
+    int value;
+
+    public Tile(int value) {
+        this.value = value;
+    }
+
+    public Tile() {
+        value = 0;
+    }
+
+    public boolean isEmpty() {
+        return value == 0;
+    }
+
+    public Color getFontColor() {
+        if (value < 16) {
+            return new Color(0x776e65);
+        } else {
+            return new Color(0xf9f6f2);
+        }
+    }
+
+    public Color getTileColor() {
+        switch (value) {
+            case 0:
+                return new Color(0xcdc1b4);
+            case 2:
+                return new Color(0xeee4da);
+            case 4:
+                return new Color(0xede0c8);
+            case 8:
+                return new Color(0xf2b179);
+            case 16:
+                return new Color(0xf59563);
+            case 32:
+                return new Color(0xf67c5f);
+            case 64:
+                return new Color(0xf65e3b);
+            case 128:
+                return new Color(0xedcf72);
+            case 256:
+                return new Color(0xedcc61);
+            case 512:
+                return new Color(0xedc850);
+            case 1024:
+                return new Color(0xedc53f);
+            case 2048:
+                return new Color(0xedc22e);
+
+            default:
+                return new Color(0xff0000);
+        }
+    }
+}
