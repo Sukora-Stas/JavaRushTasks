6fcc8b2ce067b065060d440839de725f297b2612
SukoraStas <stasiksukora@mail.ru>
2017-09-05 15:10:30 +0300

    Task --> 2712 Restaurant -- 7

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -4,4 +4,7 @@ package com.javarush.task.task27.task2712.ad;
  * Created by Sukora Stas.
  */
 public class AdvertisementManager {
+
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Sukora Stas.
  */
@@ -10,6 +13,25 @@ public class AdvertisementStorage {
         return ourInstance;
     }
 
+    private final List<Advertisement> videos = new ArrayList<>();
+
     private AdvertisementStorage() {
+
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));  //10 min
     }
+
+
+    public List list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+
+        videos.add(advertisement);
+    }
+
 }
+
