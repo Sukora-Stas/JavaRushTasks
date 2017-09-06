4327bb4b5d4bf4e7caec0007c049e1c551119d39
SukoraStas <stasiksukora@mail.ru>
2017-09-06 13:26:13 +0300

    Task --> 3513 "2048" -- 4

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sukora Stas.
  */
@@ -9,11 +11,36 @@ public class Model {
 
     private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
-    public Model(){
+    public Model() {
+        resetGameTiles();
+    }
+    protected void resetGameTiles() {
         for (int i = 0; i < FIELD_WIDTH; i++) {
             for (int j = 0; j < FIELD_WIDTH; j++) {
                 gameTiles[i][j] = new Tile();
             }
+        }
+        addTile();
+        addTile();
+    }
+
+    private ArrayList<Tile> getEmptyTiles() {
+        ArrayList<Tile> emptyTiles = new ArrayList<>();
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                if (gameTiles[i][j].isEmpty()) {
+                    emptyTiles.add(gameTiles[i][j]);
+                }
+            }
+        }
+        return emptyTiles;
+    }
+
+    private void addTile() {
+        ArrayList<Tile> emptyTiles = getEmptyTiles();
+        if (!emptyTiles.isEmpty()) {
+            int randomTileIndex = (int) (Math.random() * emptyTiles.size());
+            emptyTiles.get(randomTileIndex).value = (Math.random() < 0.9) ? 2 : 4;
         }
     }
 
