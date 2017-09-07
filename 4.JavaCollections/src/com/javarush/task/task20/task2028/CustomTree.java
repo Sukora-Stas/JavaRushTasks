b937170bf876182f9e3d70e968d001b0883e50d9
SukoraStas <stasiksukora@mail.ru>
2017-09-08 00:30:03 +0300

    Task --> 2213 Tetris -- 2

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -19,36 +19,45 @@ public class CustomTree extends AbstractList implements Cloneable, Serializable 
         list.remove("5");
         //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
     }
+
     @Override
     public String get(int index) {
         throw new UnsupportedOperationException();
     }
+
     @Override
     public int size() {
         return 0;
     }
+
     public String set(int index, String element) {
         throw new UnsupportedOperationException();
     }
+
     public void add(int index, String element) {
         throw new UnsupportedOperationException();
     }
+
     @Override
     public String remove(int index) {
         throw new UnsupportedOperationException();
     }
+
     @Override
     public boolean addAll(int index, Collection c) {
         throw new UnsupportedOperationException();
     }
+
     @Override
     public List<String> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
+
     @Override
     protected void removeRange(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
+
     static class Entry<T> implements Serializable {
         String elementName;
         int lineNumber;
--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -1,9 +1,14 @@
 package com.javarush.task.task22.task2213;
 
+
 /**
  * Created by Sukora Stas.
  */
 public class Tetris {
+
+    private Field field;
+    private Figure figure;
+
     public static void main(String[] args) {
 
     }
