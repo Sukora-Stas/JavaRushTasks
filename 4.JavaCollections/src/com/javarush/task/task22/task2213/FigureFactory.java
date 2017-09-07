46d10b9410ecb48471edd16c9df770f580674856
SukoraStas <stasiksukora@mail.ru>
2017-09-08 01:09:58 +0300

    Task --> 2213 Tetris -- 11

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/FigureFactory.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class FigureFactory {
+
+    public static Figure createRandomFigure(int x, int y) {
+        return null;
+    }
+
+}
