e6fcd70761dfc1ba819a5b6d440cb27d1d986e65
SukoraStas <stasiksukora@mail.ru>
2017-09-08 00:31:39 +0300

    Task --> 2213 Tetris -- 3

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -12,4 +12,20 @@ public class Tetris {
     public static void main(String[] args) {
 
     }
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
 }
