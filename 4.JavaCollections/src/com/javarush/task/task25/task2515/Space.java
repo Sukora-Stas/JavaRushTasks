d39e91b25e996bb1423231ab72863221374a5f20
SukoraStas <stasiksukora@mail.ru>
2017-09-09 12:49:35 +0300

    Task --> 2515 Space -- 3

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -1,10 +1,54 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sukora Stas.
  */
 public class Space {
+
+    private int width;
+    private int height;
+    private SpaceShip ship;
+
+    private ArrayList<Ufo> ufos = new ArrayList<>();
+    private ArrayList<Rocket> rockets = new ArrayList<>();
+    private ArrayList<Bomb> bombs = new ArrayList<>();
+
     public static void main(String[] args) {
 
+    }
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public ArrayList<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public ArrayList<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public ArrayList<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
     }
 }
