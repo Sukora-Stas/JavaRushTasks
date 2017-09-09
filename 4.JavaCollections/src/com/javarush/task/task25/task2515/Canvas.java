2e3ff9bf6d0b5a0df0f9bc3d1e88a761bcc7bd63
SukoraStas <stasiksukora@mail.ru>
2017-09-09 13:05:15 +0300

    Task --> 2515 Space -- 7

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -4,4 +4,26 @@ package com.javarush.task.task25.task2515;
  * Created by Sukora Stas.
  */
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height][width];
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
