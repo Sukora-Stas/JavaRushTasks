f7aa3a1beaedb5b9e6af0dc7aab8250668c77687
SukoraStas <stasiksukora@mail.ru>
2017-09-09 13:53:36 +0300

    Task --> 2515 Space -- 12

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * Created by Sukora Stas.
+ * Класс для объектов-ракета
  */
 public class Rocket extends BaseObject {
 
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -4,7 +4,21 @@ package com.javarush.task.task25.task2515;
  * Created by Sukora Stas.
  */
 public class SpaceShip extends BaseObject {
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+
+
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
     }
+
+    //вектор движения (-1 влево,+1 вправо)
+    private double dx = 0;
+
+    public void moveLeft() {
+        dx = -1;
+    }
+
+    public void moveRight() {
+        dx = 1;
+    }
+
 }
