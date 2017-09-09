0e47dbddbfc68e2e24f12c66866cab84c53bed4b
SukoraStas <stasiksukora@mail.ru>
2017-09-09 12:41:34 +0300

    Task --> 2515 Space -- 1

---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Space {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class SpaceShip {
+}
