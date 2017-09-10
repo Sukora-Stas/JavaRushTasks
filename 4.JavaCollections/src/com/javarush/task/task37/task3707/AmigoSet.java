bee4bdcdac8973846a40d1947adc5e7464ee405f
SukoraStas <stasiksukora@mail.ru>
2017-09-10 18:00:05 +0300

    Task --> 3707 AmigoSet -- 2

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,16 +1,33 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Sukora Stas.
  */
-public class AmigoSet<E> extends AbstractSet implements Serializable,Cloneable,Set {
+public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Serializable {
+
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(int capacity) {
+        map = new HashMap<>(capacity);
+    }
+
+
+    public AmigoSet(Collection<? extends E> collection){
+        map = new HashMap<>((int)Math.max(16,collection.size()/.75f));
+        addAll(collection);
+    }
+
+
     @Override
-    public Iterator iterator() {
+    public Spliterator spliterator() {
         return null;
     }
 
@@ -18,4 +35,65 @@ public class AmigoSet<E> extends AbstractSet implements Serializable,Cloneable,S
     public int size() {
         return 0;
     }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
+
