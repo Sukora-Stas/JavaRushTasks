9ee973227bfd3ea223fa90b90ad64dda1c9a4331
SukoraStas <stasiksukora@mail.ru>
2017-09-12 00:46:21 +0300

    Task --> 3310 Shortener -- 3

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,16 +1,32 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 /**
  * Created by Sukora Stas.
  */
 public class Shortener {
+    private Long lastId;
+    private StorageStrategy storageStrategy;
 
-    public Long getId(String string){
-        return null;
+    public Shortener(StorageStrategy storageStrategy) {
+        this.lastId = 0L;
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id){
-        return null;
+    public synchronized  Long getId(String string){
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        } else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+        }
+
+        return lastId;
+    }
+
+    public synchronized  String getString(Long id){
+        return storageStrategy.getValue(id);
     }
 
 }
