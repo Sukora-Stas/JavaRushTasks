fe8f2dab0040fdbac157944f4acb7beae94ff87d
SukoraStas <stasiksukora@mail.ru>
2017-09-12 00:52:07 +0300

    Task --> 3310 Shortener -- 6

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,10 +1,76 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Sukora Stas.
  */
 public class Solution {
     public static void main(String[] args) {
+        StorageStrategy strategy1 = new HashMapStorageStrategy();
+        testStrategy(strategy1, 10000);
+
+    }
+
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        Set<Long> result = new HashSet<>();
+        for (String s : strings)
+            result.add(shortener.getId(s));
+
+        return result;
+    }
+
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+        Set<String> result = new HashSet<>();
+        for (Long id : keys)
+            result.add(shortener.getString(id));
+
+        return result;
+    }
+
+    public static void testStrategy(StorageStrategy strategy, long elementsNumber) {
+        //6.2.3.1. Выводить имя класса стратегии. Имя не должно включать имя пакета.
+        System.out.println(strategy.getClass().getSimpleName());
+
+
+        //6.2.3.2. Генерировать тестовое множество строк
+        Set<String> strings = new HashSet<>();
+        Long[] elements = new Long[(int) elementsNumber];
+
+        for (int i = 0; i < elementsNumber; i++)
+            strings.add(Helper.generateRandomString());
+
+        //6.2.3.3. Создавать объект типа Shortener, используя переданную стратегию.
+        Shortener shortener = new Shortener(strategy);
+
+        //6.2.3.4. Замерять и выводить время необходимое для отработки метода getIds для заданной стратегии
+        Date startDateTime = new Date();
+        Set<Long> ids = getIds(shortener, strings);
+        Date finishDateTime = new Date();
+
+        long deltaTime = finishDateTime.getTime() - startDateTime.getTime();
+        Helper.printMessage(Long.toString(deltaTime));
+
+        //6.2.3.5. Замерять и выводить время необходимое для отработки метода getStrings для заданной стратегии
+        startDateTime = new Date();
+        Set<String> strs = getStrings(shortener, ids);
+        finishDateTime = new Date();
+
+        deltaTime = finishDateTime.getTime() - startDateTime.getTime();
+        Helper.printMessage(Long.toString(deltaTime));
+
+
+        //6.2.3.6. Сравнивать одинаковое ли содержимое множества строк, которое было сгенерировано и множества, которое было возвращено методом getStrings.
+        if (strings.equals(strs))
+            Helper.printMessage("Тест пройден.");
+        else
+            Helper.printMessage("Тест не пройден.");
+
 
     }
 }
