82b481f7801bf88274016104aa1d808a20c5fac7
SukoraStas <stasiksukora@mail.ru>
2017-09-13 12:50:40 +0300

    Task --> 3913 ParserLogs -- start

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3906/Switchable.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task39.task3906;
+
+/**
+ * Created by Sukora Stas.
+ */
+public interface Switchable {
+    boolean isOn();
+
+    void turnOff();
+
+    void turnOn();
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task39.task3913;
+
+public class LogParser {
+}
