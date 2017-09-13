8d853f2db375868d03fdc5288d9ff7ce0215d1de
SukoraStas <stasiksukora@mail.ru>
2017-09-13 14:13:02 +0300

    Task --> 2810 Aggregator -- 4

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -4,4 +4,6 @@ package com.javarush.task.task28.task2810.model;
  * Created by Sukora Stas.
  */
 public class HHStrategy implements Strategy {
+    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";;
+
 }
