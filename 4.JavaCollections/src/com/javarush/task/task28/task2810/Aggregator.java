86fe79417ea42b23499295ae365af414b64b24d8
SukoraStas <stasiksukora@mail.ru>
2017-09-13 14:00:54 +0300

    Task --> 2810 Aggregator -- 2

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,10 +1,17 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.model.Strategy;
+
 /**
  * Created by Sukora Stas.
  */
 public class Aggregator {
     public static void main(String[] args) {
+        Provider provider = new Provider(new Strategy() {
+        });
+        Controller controller = new Controller(provider);
+        System.out.println(controller);
 
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task28.task2810;
+
+
+import com.javarush.task.task28.task2810.model.Provider;
+
+import java.util.Arrays;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class Controller
+{
+    private Provider[] providers;
+
+    public Controller(Provider... providers)
+    {
+        if (providers == null || providers.length == 0)
+        {
+            throw new IllegalArgumentException();
+        }
+        this.providers = providers;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Controller{" +
+                "providers=" + Arrays.toString(providers) +
+                '}';
+    }
+}
