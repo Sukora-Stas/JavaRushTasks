732412df6909d10da57ba7248bd1e36f666807da
SukoraStas <stasiksukora@mail.ru>
2017-09-14 00:49:34 +0300

    Task --> 2613 CashMachine -- 2

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,7 +1,32 @@
 package com.javarush.task.task26.task2613;
 
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Sukora Stas.
  */
 public class ConsoleHelper {
+
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws InterruptOperationException {
+        String input = "";
+        try {
+            input = bis.readLine();
+            if (input.equalsIgnoreCase("exit")) {
+                throw new InterruptOperationException();
+            }
+        } catch (IOException ignored) {}
+
+        return input;
+    }
+
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/exception/InterruptOperationException.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task26.task2613.exception;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class InterruptOperationException extends Exception {
+}
