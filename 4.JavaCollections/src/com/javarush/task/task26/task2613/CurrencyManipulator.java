7112318600e6db6381e1b02b6ddd67769ef38160
SukoraStas <stasiksukora@mail.ru>
2017-09-14 17:22:25 +0300

    Task --> 2613 CashMachine -- 10

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task26.task2613;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 /**
  * Created by Sukora Stas.
@@ -40,5 +41,62 @@ public class CurrencyManipulator
 
     public boolean hasMoney() {
         return !denominations.isEmpty();
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+
+        return getTotalAmount() >= expectedAmount;
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        int sum = expectedAmount;
+        HashMap<Integer, Integer> temp = new HashMap<>();
+        temp.putAll(denominations);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> pair : temp.entrySet())
+            list.add(pair.getKey());
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        TreeMap<Integer, Integer> result = new TreeMap<>(new Comparator<Integer>()
+        {
+            @Override
+            public int compare(Integer o1, Integer o2)
+            {
+                return o2.compareTo(o1);
+            }
+        });
+
+        for (Integer aList : list) {
+            int key = aList;
+            int value = temp.get(key);
+            while (true) {
+                if (sum < key || value <= 0) {
+                    temp.put(key, value);
+                    break;
+                }
+                sum -= key;
+                value--;
+
+                if (result.containsKey(key))
+                    result.put(key, result.get(key) + 1);
+                else
+                    result.put(key, 1);
+            }
+        }
+
+        if (sum > 0)
+            throw new NotEnoughMoneyException();
+        else
+        {
+            for (Map.Entry<Integer, Integer> pair : result.entrySet())
+                ConsoleHelper.writeMessage("\t" + pair.getKey() + " - " + pair.getValue());
+
+            denominations.clear();
+            denominations.putAll(temp);
+            ConsoleHelper.writeMessage("Transaction was successful!");
+        }
+        return result;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,11 +1,50 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.ConcurrentModificationException;
+import java.util.Map;
+
 /**
  * Created by Sukora Stas.
  */
-class WithdrawCommand implements Command {
+class WithdrawCommand implements Command
+{
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException
+    {
+        String currencyCode = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator cm = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
 
+        boolean flag = true;
+        while (flag) {
+            System.out.println("Enter amount of money for withdraw");
+            String temp = ConsoleHelper.readString();
+            if (temp.matches("\\d+")) {
+                int amount = Integer.valueOf(temp);
+                if (cm.isAmountAvailable(amount)) {
+                    Map<Integer, Integer> wa;
+                    try
+                    {
+                        wa = cm.withdrawAmount(amount);
+                        for (Map.Entry<Integer, Integer> pair : wa.entrySet()) {
+                            System.out.println("\t" + pair.getKey() + " - " + pair.getValue());
+                        }
+                    }
+                    catch (NotEnoughMoneyException e)
+                    {
+                        System.out.println("Not enough money");
+                    }
+                    catch (ConcurrentModificationException e) {}
+                    flag = false;
+                }
+            } else {
+                System.out.println("Incorrect data");
+            }
+        }
     }
 }
