c1c03cde8a54d4cde1381e1df34be9e34ce8dcdc
SukoraStas <stasiksukora@mail.ru>
2017-09-15 13:58:33 +0300

    Task --> 3410 Sokoban -- 15

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -37,5 +37,104 @@ public class Model {
 
     public void move(Direction direction) {
 
+        Player player = gameObjects.getPlayer();
+
+        if (checkWallCollision(player, direction)) {
+            return;
+        }
+        if (checkBoxCollisionAndMoveIfAvaliable(direction)) {
+            return;
+        }
+
+        int sellSize = FIELD_SELL_SIZE;
+        switch (direction) {
+            case LEFT:
+                player.move(-sellSize, 0);
+                break;
+            case RIGHT:
+                player.move(sellSize, 0);
+                break;
+            case UP:
+                player.move(0, -sellSize);
+                break;
+            case DOWN:
+                player.move(0, sellSize);
+        }
+        checkCompletion();
+    }
+
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+
+        for (Wall wall : gameObjects.getWalls()) {
+
+            if (gameObject.isCollision(wall, direction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean checkBoxCollisionAndMoveIfAvaliable(Direction direction) {
+
+        Player player = gameObjects.getPlayer();
+
+        // найдем во что уперся игрок
+        GameObject stoped = null;
+        for (GameObject gameObject : gameObjects.getAll()) {
+            if (!(gameObject instanceof Player) && !(gameObject instanceof Home) && player.isCollision(gameObject, direction)) {
+                stoped = gameObject;
+            }
+        }
+        //свободное место или дом
+        if ((stoped == null)) {
+            return false;
+        }
+        if (stoped instanceof Box) {
+            Box stopedBox = (Box) stoped;
+            if (checkWallCollision(stopedBox, direction)) {
+                return true;
+            }
+            for (Box box : gameObjects.getBoxes()) {
+                if (stopedBox.isCollision(box, direction)) {
+                    return true;
+                }
+            }
+            switch (direction) {
+                case LEFT:
+                    stopedBox.move(-FIELD_SELL_SIZE, 0);
+                    break;
+                case RIGHT:
+                    stopedBox.move(FIELD_SELL_SIZE, 0);
+                    break;
+                case UP:
+                    stopedBox.move(0, -FIELD_SELL_SIZE);
+                    break;
+                case DOWN:
+                    stopedBox.move(0, FIELD_SELL_SIZE);
+            }
+        }
+        return false;
+
+    }
+
+    public void checkCompletion() {
+
+        boolean yes = true;
+
+        for (Home home : gameObjects.getHomes()) {
+            boolean currentyes = false;
+
+            for (Box box : gameObjects.getBoxes()) {
+                if ((box.getX() == home.getX()) && (box.getY() == home.getY()))
+                    currentyes = true;
+            }
+
+            if (!currentyes) yes = false;
+        }
+
+        if (yes)
+            eventListener.levelCompleted(currentLevel);
     }
 }
