09a648ac60c436896cb2ff95638030c563eca32f
SukoraStas <stasiksukora@mail.ru>
2017-09-16 01:17:28 +0300

    Task --> 2802 DONE

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -6,16 +6,12 @@ import java.util.concurrent.CountDownLatch;
 CountDownLatch
 */
 public abstract class Solution {
-
     private final CountDownLatch latch = new CountDownLatch(1);
 
-
     public void someMethod() throws InterruptedException {
-
         latch.await();
         retrieveValue();
         latch.countDown();
-
     }
 
     abstract void retrieveValue();
---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2802/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task28.task2802;
+
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Пишем свою ThreadFactory
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        class EmulateThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulateThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulateThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+        static final AtomicInteger poolNumber = new AtomicInteger(1);
+        final ThreadGroup group;
+        final AtomicInteger threadNumber = new AtomicInteger(1);
+        final String namePrefix;
+
+        AmigoThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+            namePrefix = group.getName() + "-pool-" + poolNumber.getAndIncrement() + "-thread-";
+        }
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement() ,0);
+            if (t.isDaemon()) t.setDaemon(false);
+            if (t.getPriority() != Thread.NORM_PRIORITY) t.setPriority(Thread.NORM_PRIORITY);
+            return t;
+        }
+    }
+}
