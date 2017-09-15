a0fe2eee7270feb4d630a7b99d76f36653e31209
SukoraStas <stasiksukora@mail.ru>
2017-09-15 12:20:47 +0300

    Task --> 3410 Sokoban -- 2

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/GameObject.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.awt.*;
+
+/**
+ * Created by Sukora Stas.
+ */
+public abstract class GameObject {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public GameObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.width = Model.FIELD_SELL_SIZE;
+        this.height = Model.FIELD_SELL_SIZE;
+    }
+
+    public abstract void draw(Graphics graphics);
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -4,4 +4,5 @@ package com.javarush.task.task34.task3410.model;
  * Created by Sukora Stas.
  */
 public class Model {
+    public static final int FIELD_SELL_SIZE = 20;
 }
