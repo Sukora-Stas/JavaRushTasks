a14465549ef233056c83cf245be56e8c1b337886
SukoraStas <stasiksukora@mail.ru>
2017-09-15 13:02:50 +0300

    Task --> 3410 Sokoban -- 11

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -2,14 +2,36 @@ package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
 
+import java.nio.file.Paths;
+
 /**
  * Created by Sukora Stas.
  */
 public class Model {
     public static final int FIELD_SELL_SIZE = 20;
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("..\\res\\levels.txt"));
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        this.gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel = currentLevel + 1;
+        restartLevel(currentLevel);
     }
 }
