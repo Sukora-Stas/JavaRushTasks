04675b6ce7593815f8951a3d5ad1e9e240b0b676
SukoraStas <stasiksukora@mail.ru>
2017-09-19 00:06:03 +0300

    Task --> 3707 AmigoSet -- DONE

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,64 +1,74 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
 /**
  * Created by Sukora Stas.
  */
-public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Serializable {
+public class AmigoSet<E> extends AbstractSet<E> implements Set<E>,Cloneable,Serializable
+{
+    private final static Object PRESENT = new Object();
+    private transient HashMap<E,Object> map;
 
-    private static final Object PRESENT = new Object();
-    private transient HashMap<E, Object> map;
-
-    public AmigoSet() {
-        map = new HashMap<>();
+    public AmigoSet (Collection<? extends E> collection)
+    {
+        map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
+        addAll(collection);
     }
 
     public AmigoSet(int capacity) {
         map = new HashMap<>(capacity);
     }
 
-
-    public AmigoSet(Collection<? extends E> c) {
-        map = new HashMap<>(Math.max((int) (c.size() / .75f) + 1, 16));
-        addAll(c);
+    public AmigoSet() {
+        map = new HashMap<>();
     }
 
+    public AmigoSet(int initialCapacity, float loadFactor) {
+        map = new HashMap<>(initialCapacity, loadFactor);
+    }
 
     @Override
-    public Spliterator spliterator() {
-        return null;
+    public Object clone() throws InternalError{
+        try {
+            AmigoSet<E> newSet = (AmigoSet<E>) super.clone();
+            newSet.map = (HashMap<E, Object>) map.clone();
+            return newSet;
+        } catch (Exception e) {
+            throw new InternalError(e);
+        }
+    }
+
+    private void writeObject(ObjectOutputStream s) throws IOException {
+        s.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+        s.defaultReadObject();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return map.isEmpty();
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
+        return map.containsKey(o);
     }
 
     @Override
@@ -68,33 +78,14 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Se
 
     @Override
     public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection c) {
-        return false;
+        return map.keySet().remove(o);
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
-}
 
+
+}
 
