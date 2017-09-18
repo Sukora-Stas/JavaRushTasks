18c439b72a54f8a546709ec8955abb053c15521d
SukoraStas <stasiksukora@mail.ru>
2017-09-19 00:01:10 +0300

    Task --> 3012

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
@@ -11,6 +11,42 @@ public class Solution {
     }
 
     public void createExpression(int number) {
-        //напишите тут ваш код
+        int[] arr={1, 3, 9, 27, 81, 243, 729, 2187};
+        String s="";
+        int nn=number;
+        while (nn>0)
+        {
+            if (nn%3==0)
+            {
+                s=s+"0";
+                nn/=3;
+            }
+            else if (nn%3==1)
+            {
+                s=s+"+";
+                nn/=3;
+            }
+            else if (nn%3==2)
+            {
+                s=s+"-";
+                nn/=3;
+                nn++;
+            }
+        }
+        int sum=0;
+        String str="";
+        for (int i=0;i<s.length(); i++)
+        {
+            if (s.charAt(i)=='+') {
+                str+=" + "+arr[i];
+                sum+=arr[i];
+            }
+            else if (s.charAt(i)=='-')
+            {
+                str+=" - "+arr[i];
+                sum-=arr[i];
+            }
+        }
+        System.out.println(sum+" ="+str);
     }
 }
