39e676d43cc6f05192819c21b7260e4254928918
SukoraStas <stasiksukora@mail.ru>
2017-09-18 21:31:28 +0300

    Task --> 2707 DONE

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -5,30 +5,55 @@ package com.javarush.task.task27.task2707;
 */
 public class Solution {
     public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
-        synchronized (obj1) {
-            synchronized (obj2) {
+        int lock1 = obj1.hashCode();
+        int lock2 = obj2.hashCode();
+        Object firstLock = lock1 > lock2 ? obj1 : obj2;
+        Object secondLock = lock1 > lock2 ? obj2 : obj1;
+        synchronized (firstLock) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+            }
+            synchronized (secondLock) {
                 System.out.println(obj1 + " " + obj2);
             }
         }
     }
 
-    public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception {
-        //do something here
-        if (o1.hashCode() < o2.hashCode()) {
-            synchronized (o1) {
-                synchronized (o2) {
-                    return true;
-                }
-            }
-        }
-        else {
-            synchronized (o2) {
+    public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception
+    {
+        new Thread() {
+            @Override
+            public void run() {
                 synchronized (o1) {
-                    return false;
+                    try {
+                        sleep(5000);
+                    } catch (InterruptedException e) {
+
+                    }
                 }
             }
-        }
+        }.start();
 
+        Thread thread2=new Thread() {
+            @Override
+            public void run() {
+                synchronized (o1)
+                {
+                    synchronized (o2)
+                    {
+                        solution.someMethodWithSynchronizedBlocks(o1, o2);
+                    }
+                }
+            }
+        };
+        thread2.start();
+        return Thread.State.BLOCKED.equals(thread2.getState());
+    }
+
+    public static Thread.State waitAndGetState(Thread thread) throws InterruptedException {
+        Thread.sleep(300);
+        return thread.getState();
     }
 
     public static void main(String[] args) throws Exception {
