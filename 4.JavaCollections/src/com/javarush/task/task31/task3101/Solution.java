fa834b8d7faf14a4ef5b0712bc30f14f6a0ad138
Sukora Stas <stasiksukora@mail.ru>
2017-10-24 22:22:57 +0300

    Task --> 3101

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task31.task3101;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/*
+Проход по дереву файлов
+*/
+public class Solution {
+    private static ArrayList<File> fileList = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        File path = new File(args[0]);
+        File resultFileAbsolutePath = new File(args[1]);
+        File allFilesContent = new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt");
+        FileUtils.renameFile(resultFileAbsolutePath, allFilesContent);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(allFilesContent)) {
+
+            fillFileList(path.getPath());
+            fileList.sort(new FileNameComparator());
+
+            for (File file : fileList) {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                while (fileInputStream.available() > 0) {
+                    fileOutputStream.write(fileInputStream.read());
+                }
+                fileOutputStream.write(System.lineSeparator().getBytes());
+                fileOutputStream.flush();
+
+                fileInputStream.close();
+            }
+        }
+    }
+
+    public static void deleteFile(File file) {
+        if (!file.delete()) System.out.println("Can not delete file with name " + file.getName());
+    }
+
+    //Рекурсивно пробегаем поддиректории и заполняем список файлов
+    private static void fillFileList(String path) {
+        File[] files = new File(path).listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                fillFileList(file.getAbsolutePath());
+                continue;
+            }
+            if (file.length() > 50)
+                FileUtils.deleteFile(file);
+            else
+                fileList.add(file);
+        }
+    }
+}
+
+//Компаратор для сравнения
+class FileNameComparator implements Comparator<File> {
+    public int compare(File first, File second) {
+        return first.getName().compareTo(second.getName());
+    }
+}
