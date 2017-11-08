a138a9bac5f2ab7d9c8e10f5018d58faff4f912d
Sukora Stas <stasiksukora@mail.ru>
2017-11-08 21:27:45 +0300

    Task --> 3105

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task31.task3105;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+/* 
+Добавление файла в архив
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String fileName = args[0];
+        String zipFileName = args[1];
+        File file = new File(fileName);
+
+        Map<String, ByteArrayOutputStream> archivedFiles = new HashMap<>();
+        try (ZipInputStream zipReader = new ZipInputStream(new FileInputStream(zipFileName))) {
+            ZipEntry entry;
+            while ((entry = zipReader.getNextEntry()) != null) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int count = 0;
+                while ((count = zipReader.read(buffer)) != -1)
+                    byteArrayOutputStream.write(buffer, 0, count);
+
+                archivedFiles.put(entry.getName(), byteArrayOutputStream);
+            }
+        }
+
+        try (ZipOutputStream zipWriter = new ZipOutputStream(new FileOutputStream(zipFileName))) {
+            for (Map.Entry<String, ByteArrayOutputStream> pair : archivedFiles.entrySet()) {
+                if (pair.getKey().substring(pair.getKey().lastIndexOf("/") + 1).equals(file.getName())) continue;
+                zipWriter.putNextEntry(new ZipEntry(pair.getKey()));
+                zipWriter.write(pair.getValue().toByteArray());
+            }
+
+            ZipEntry zipEntry = new ZipEntry("new/" + file.getName());
+            zipWriter.putNextEntry(zipEntry);
+            Files.copy(file.toPath(), zipWriter);
+        }
+    }
+}
