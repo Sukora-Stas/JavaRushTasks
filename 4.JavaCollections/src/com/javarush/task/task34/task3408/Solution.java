4d458b45646dff4cc82ad1b282c099e5182938ab
Sukora Stas <stasiksukora@mail.ru>
2018-01-15 07:27:32 +0300

    Task --> 3408

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3408/Solution.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task34.task3408;
+
+/* 
+Кэширование
+*/
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        SomeKey someKey = new SomeKey();
+        someKey.name = "test";
+
+        SomeKey someKeyNew = new SomeKey();
+        someKeyNew.name = "testNew";
+
+        SomeValue value = new SomeValue(someKey);
+
+        Cache<SomeKey, SomeValue> cache = new Cache<>();
+        cache.put(value);
+
+        SomeValue valueFromCacheExisted = cache.getByKey(someKey, SomeValue.class);
+        System.out.println(valueFromCacheExisted);
+
+        SomeValue valueFromCacheNew = cache.getByKey(someKeyNew, SomeValue.class);
+        System.out.println(valueFromCacheNew);
+
+        System.out.println(cache.size());
+        /* expected output:
+        SomeValue{myKey=SomeKey{name='test'}}
+        SomeValue{myKey=SomeKey{name='testNew'}}
+        2
+         */
+    }
+
+    public static class SomeKey {
+        String name;
+
+        @Override
+        public String toString() {
+            return "SomeKey{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    public static class SomeValue {
+        private SomeKey myKey;
+
+        public SomeValue() {
+        }
+
+        public SomeValue(SomeKey myKey) {        //use this constructor
+            this.myKey = myKey;
+        }
+
+        private SomeKey getKey() {
+            return myKey;
+        }
+
+        @Override
+        public String toString() {
+            return "SomeValue{" +
+                    "myKey=" + myKey +
+                    '}';
+        }
+    }
+}
