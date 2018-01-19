445f22e5cf8c219641b9a15d1ede88173f025a01
Sukora Stas <stasiksukora@mail.ru>
2018-01-19 07:22:54 +0300

    Task --> 3501

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task35.task3501;
+/* 
+Вызов статического метода
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Number number = GenericStatic.<Number>someStaticMethod(new Integer(3));
+    }
+}
