1d87aef5f0ff4bb2fe5a2179e5c6fea3ed53b316
Sukora Stas <stasiksukora@mail.ru>
2018-02-22 07:11:42 +0300

    Task --> 3509

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task35.task3509;
+
+import java.util.*;
+
+
+/* 
+Collections & Generics
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        //ArrayList<Integer> tst = newArrayList (new Integer(10), new Integer(20));
+    }
+
+    public static <T> ArrayList<T> newArrayList(T... elements) {
+        //напишите тут ваш код
+        ArrayList<T> res = new ArrayList<>();
+        for (T elm : elements)
+            res.add(elm);
+
+        return res;
+    }
+
+    public static <T> HashSet<T> newHashSet(T... elements) {
+        //напишите тут ваш код
+        HashSet<T> res = new HashSet<>();
+        for (T elm : elements)
+            res.add(elm);
+
+        return res;
+    }
+
+    public static <K, V> HashMap<K, V> newHashMap(List<? extends K> keys, List<? extends V> values) {
+        //напишите тут ваш код
+        if (keys.size() != values.size())
+            throw new IllegalArgumentException();
+
+        HashMap<K, V> res = new HashMap<>();
+        for (int i = 0; i < keys.size(); i++) {
+            res.put(keys.get(i), values.get(i));
+        }
+
+        return res;
+    }
+}
