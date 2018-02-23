95ea47d95f2a6e6cd3e76af1231787698e47519c
Sukora Stas <stasiksukora@mail.ru>
2018-02-23 17:19:18 +0300

    Task --> 3509

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3510/House.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task35.task3510;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Вход воспрещен!
+*/
+public class House <T> {
+
+    private List<T> residents = new ArrayList();
+
+    public void enter(T resident) {
+        residents.add(resident);
+//        checkConflicts();
+    }
+
+    public void leave(T resident) {
+        residents.remove(resident);
+    }
+
+//    private void checkConflicts() {
+//        boolean conflict = false;
+//        for (Object resident : residents) {
+//            if (resident instanceof Dog) {
+//                conflict = true;
+//            }
+//        }
+//
+//        if (conflict) {
+//            Iterator iterator = residents.iterator();
+//            while (iterator.hasNext()) {
+//                Object resident = iterator.next();
+//                if (resident instanceof Cat) {
+//                    iterator.remove();
+//                }
+//            }
+//        }
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("В доме находятся:\n");
+        for (Object resident : residents) {
+            builder.append(resident.toString()).append("\n");
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Dog bruno = new Dog("Bruno");
+        Puppy larsik = new Puppy("Larsik");
+        Cat barsik = new Cat("Barsik");
+        Kitten keksik = new Kitten("Keksik");
+
+        House<Dog> dogHouse = new House();
+        dogHouse.enter(bruno);
+        dogHouse.enter(larsik);
+//        dogHouse.enter(barsik);
+        System.out.println(dogHouse.toString());
+
+        House<Cat> catHouse = new House();
+        catHouse.enter(barsik);
+        catHouse.enter(keksik);
+//        catHouse.enter(bruno);
+        System.out.println(catHouse.toString());
+    }
+}
