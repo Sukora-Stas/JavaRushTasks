ddfd154e0c937b9b3afe1bc43aad5ade263f22c6
Sukora Stas <stasiksukora@mail.ru>
2018-03-26 22:09:05 +0300

    Task --> 3708

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3708/retrievers/CachingProxyRetriever.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task37.task3708.retrievers;
+
+import com.javarush.task.task37.task3708.cache.LRUCache;
+import com.javarush.task.task37.task3708.storage.Storage;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class CachingProxyRetriever implements Retriever{
+    private Storage storage;
+    private OriginalRetriever originalRetriever;
+    private LRUCache<Long, Object> lruCache;
+
+    public CachingProxyRetriever(Storage storage) {
+        this.storage = storage;
+        this.originalRetriever = new OriginalRetriever(storage);
+        lruCache = new LRUCache<>(16);
+    }
+
+    @Override
+    public Object retrieve(long id) {
+        Object obj = lruCache.find(id);
+        if (obj == null) {
+            obj = originalRetriever.retrieve(id);
+            lruCache.set(id, obj);
+        }
+        return obj;
+    }
+}
