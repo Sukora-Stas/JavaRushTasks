db82a931b7b12f6fc796e6120a4fde9f6ad8276f
Sukora Stas <stasiksukora@mail.ru>
2018-03-28 07:28:59 +0300

    Task --> 3713

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3713/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task37.task3713;
+
+import com.javarush.task.task37.task3713.space.crew.AbstractCrewMember;
+import com.javarush.task.task37.task3713.space.SpaceShip;
+
+/* 
+Chain of Responsibility
+*/
+public class Solution {
+    public static void main(String[] args) {
+        SpaceShip spaceShip = new SpaceShip();
+        AbstractCrewMember crewMember = spaceShip.getCrewChain();
+
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.EXPERT, "ATTACK");
+        System.out.println("-----------------------------------------");
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.NOVICE, "WASH THE FLOOR");
+        System.out.println("-----------------------------------------");
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.INTERMEDIATE, "CHECK ENGINE");
+        System.out.println("-----------------------------------------");
+        crewMember.handleRequest(AbstractCrewMember.CompetencyLevel.ADVANCED, "SET ROUTE");
+    }
+}
