6de0900d9711b2e01e9cb7e0dcb4c469f2030cc8
Sukora Stas <stasiksukora@mail.ru>
2018-03-29 07:35:00 +0300

    Task --> 3804

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3804/ExceptionFactory.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task38.task3804;
+
+/**
+ * Created by Sukora Stas.
+ */
+class ExceptionFactory {
+    public static Throwable getException(Enum type) {
+        if (type == null)
+            return new IllegalArgumentException();
+
+        String message = type.name().charAt(0) + type.name().substring(1).toLowerCase().replace("_", " ");
+
+        if (type instanceof ExceptionApplicationMessage)
+            return new Exception(message);
+
+        if (type instanceof ExceptionDBMessage)
+            return new RuntimeException(message);
+
+        if (type instanceof ExceptionUserMessage)
+            return new Error(message);
+
+        return new IllegalArgumentException();
+    }
+}
