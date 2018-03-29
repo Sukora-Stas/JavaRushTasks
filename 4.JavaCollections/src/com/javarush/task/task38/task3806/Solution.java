c073f17b0c05a044fa826bbed24992c500a48022
Sukora Stas <stasiksukora@mail.ru>
2018-03-29 07:35:22 +0300

    Task --> 3806

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3806/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task38.task3806;
+
+/* 
+Улучшения в Java 7 (try-with-resources)
+*/
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Solution {
+    public void printFile1() throws IOException {
+        //FileInputStream fileInputStream = null;
+
+        try (FileInputStream fileInputStream = new FileInputStream("file.txt")) {
+            int data = fileInputStream.read();
+            while (data != -1) {
+                System.out.println(data);
+                data = fileInputStream.read();
+            }
+        }
+    }
+
+    public void printFile2() throws IOException {
+        // FileInputStream fileInputStream = null;
+        // BufferedInputStream bufferedInputStream = null;
+        try (
+                FileInputStream fileInputStream = new FileInputStream("file.txt");
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        ) {
+            int data = bufferedInputStream.read();
+            while (data != -1) {
+                System.out.println(data);
+                data = bufferedInputStream.read();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
