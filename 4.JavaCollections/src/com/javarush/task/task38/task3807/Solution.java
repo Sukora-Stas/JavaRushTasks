f570bee79f4f15dd4cc32ef95560367c2abac91e
Sukora Stas <stasiksukora@mail.ru>
2018-03-29 07:35:34 +0300

    Task --> 3807

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3807/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task38.task3807;
+
+/* 
+Предопределенные типы аннотаций (Predefined Annotation Types)
+*/
+
+@Deprecated
+public class Solution {
+
+    @Deprecated
+    private String[] arguments;
+
+    @SafeVarargs
+    @Deprecated
+    public Solution(String... arguments) {
+        this.arguments = arguments;
+    }
+
+    @Deprecated
+    public void voidMethod() throws Exception {
+    }
+
+    @Deprecated
+    public static void main(String[] args) throws Exception {
+        new Solution().new SubSolution().voidMethod();
+    }
+
+    @Deprecated
+    class SubSolution extends Solution {
+
+        @Override
+        @Deprecated
+        public void voidMethod() throws Exception {
+            super.voidMethod();
+        }
+    }
+}
