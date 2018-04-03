97a326dba988c6a561ad842798d5ebed9a172cc0
Sukora Stas <stasiksukora@mail.ru>
2018-04-03 07:06:54 +0300

    Task --> 4004

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4004/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task40.task4004;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Принадлежность точки многоугольнику
+*/
+
+class Point {
+    public int x;
+    public int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Point> polygon = new ArrayList<>();
+        polygon.add(new Point(0, 0));
+        polygon.add(new Point(0, 10));
+        polygon.add(new Point(10, 10));
+        polygon.add(new Point(10, 0));
+
+        System.out.println(isPointInPolygon(new Point(5, 5), polygon));
+        System.out.println(isPointInPolygon(new Point(100, 100), polygon));
+    }
+
+    public static boolean isPointInPolygon(Point point, List<Point> polygon) {
+        //напишите тут ваш код
+
+        boolean res = false;
+        int j=polygon.size()-1;
+
+        for (int i = 0; i < polygon.size(); i++) {
+            if ((((polygon.get(i).y < point.y) && (point.y < polygon.get(j).y)) || ((polygon.get(j).y <= point.y) && (point.y < polygon.get(i).y))) &&
+                    (point.x > (polygon.get(j).x - polygon.get(i).x) * (point.y - polygon.get(i).y) / (polygon.get(j).y - polygon.get(i).y) + polygon.get(i).x))
+                res = !res;
+            j=i;
+        }
+
+        return res;
+    }
+
+}
+
